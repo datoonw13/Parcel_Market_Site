ea5ed1ee3a2277ca2a735ca2a9392a929ca83322
LGabelashvili <gabelashvili1999@gmail.com>
2024-03-24 21:50:53 +0400

    add home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import LandingMain from "@/components/landing/LandingMain";
 import LandingHeader from "@/components/landing/header/LandingHeader";
 
 const Landing = () => (
   <>
-    <section className="h-screen bg-neutral-400">
+    <section className="h-screen bg-neutral-400 grid" style={{ gridTemplateRows: "min-content 1fr" }}>
       <LandingHeader />
+      <LandingMain />
     </section>
+    <section className="py-20 bg-green-400 relative">asas</section>
   </>
 );
 
