d6e70db59affbe7da3d4eb97024437c93037a11e
LGabelashvili <gabelashvili1999@gmail.com>
2024-03-24 20:33:36 +0400

    fix lint bug

--- src/icons/BurgerIcon.tsx
+++ src/icons/BurgerIcon.tsx
@@ -1,8 +1,8 @@
 const BurgerIcon = () => (
   <svg viewBox="0 0 40 27" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <line y1="1.51123" x2="40" y2="1.51123" stroke="#044019" stroke-width="2" />
-    <line y1="13.5112" x2="40" y2="13.5112" stroke="#044019" stroke-width="2" />
-    <line y1="25.5112" x2="40" y2="25.5112" stroke="#044019" stroke-width="2" />
+    <line y1="1.51123" x2="40" y2="1.51123" stroke="#044019" strokeWidth="2" />
+    <line y1="13.5112" x2="40" y2="13.5112" stroke="#044019" strokeWidth="2" />
+    <line y1="25.5112" x2="40" y2="25.5112" stroke="#044019" strokeWidth="2" />
   </svg>
 );
 export default BurgerIcon;
