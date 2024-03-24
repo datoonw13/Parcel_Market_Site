21bcf31f503faec559d64ed0dbf3a826d5c72939
LGabelashvili <gabelashvili1999@gmail.com>
2024-03-24 20:43:18 +0400

    fix paddings

--- src/components/landing/header/LandingHeader.tsx
+++ src/components/landing/header/LandingHeader.tsx
@@ -3,7 +3,7 @@ import LandingDesktopNavigation from "./LandingDesktopNavigation";
 import LandingMobileNav from "./LandingMobileNav";
 
 const LandingHeader = () => (
-  <div className="px-[20px] md:px-[40px] lg:px-[60px] xl:px-[80px] 2xl:px-[100px] py-6 flex space-between gap-4 w-full items-center">
+  <div className="px-[10px] md:px-[20px] lg:px-[40px] xl:px-[80px] 2xl:px-[100px] pt-10 md:pt-6 py-6 flex space-between gap-4 w-full items-center">
     <div className="min-w-[145px] w-[145px] lg:w-[180px] 2xl:w-[220px] h-[40px] lg:h-[60px] flex cursor-pointer">
       <Logo />
     </div>
