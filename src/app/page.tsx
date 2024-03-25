85a94df6db99bda1648fb3bb47fa30a3e1ba6f71
LGabelashvili <gabelashvili1999@gmail.com>
2024-03-25 11:47:26 +0400

    hide sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import LandingFeedBacks from "@/components/landing/LandingFeedBacks";
+// import LandingFeedBacks from "@/components/landing/LandingFeedBacks";
 import LandingMain from "@/components/landing/LandingMain";
-import LandingOffer from "@/components/landing/LandingOffer";
+// import LandingOffer from "@/components/landing/LandingOffer";
 import LandingHeader from "@/components/landing/header/LandingHeader";
 
 const Landing = () => (
@@ -9,12 +9,12 @@ const Landing = () => (
       <LandingHeader />
       <LandingMain />
     </section>
-    <section className="py-10 md:py-12 lg:py-16 xl:py-18 2xl:py-24">
+    {/* <section className="py-10 md:py-12 lg:py-16 xl:py-18 2xl:py-24">
       <LandingOffer />
     </section>
     <section>
       <LandingFeedBacks />
-    </section>
+    </section> */}
   </>
 );
 
