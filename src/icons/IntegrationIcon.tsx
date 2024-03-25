f21bd80f5a91acc19587900850297cb940d63516
LGabelashvili <gabelashvili1999@gmail.com>
2024-03-25 11:36:55 +0400

    fix stroke width

--- src/icons/IntegrationIcon.tsx
+++ src/icons/IntegrationIcon.tsx
@@ -1,35 +1,35 @@
 const IntegrationIcon = () => (
   <svg viewBox="0 0 65 53" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g clip-path="url(#clip0_158_1319)">
-      <path d="M0.339966 16.9565H64.34" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M0.339966 21.9565H7.33997" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M0.339966 26.9565H5.33997" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M0.339966 41.9565H7.33997" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M51.34 21.9565H32.34H13.34" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M64.34 21.9565H57.34" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M49.34 26.9565H32.34H15.34" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M64.34 26.9565H59.34" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M43.34 31.9565H32.34H21.34" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M43.34 36.9565H32.34H21.34" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M51.34 41.9565H32.34H13.34" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M64.34 41.9565H57.34" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M0.339966 46.9565H64.34" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M0.339966 11.9565H64.34" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M64.34 51.9565H0.339966" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M64.34 7.8166L47.81 1.7966C46.33 1.2566 44.7 1.2566 43.22 1.7966L32.35 5.7566" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M38.01 7.8166L21.47 1.7966C19.99 1.2566 18.36 1.2566 16.88 1.7966L0.339966 7.8166" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M10.34 36.3164V42.9564" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
+      <path d="M0.339966 16.9565H64.34" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M0.339966 21.9565H7.33997" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M0.339966 26.9565H5.33997" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M0.339966 41.9565H7.33997" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M51.34 21.9565H32.34H13.34" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M64.34 21.9565H57.34" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M49.34 26.9565H32.34H15.34" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M64.34 26.9565H59.34" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M43.34 31.9565H32.34H21.34" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M43.34 36.9565H32.34H21.34" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M51.34 41.9565H32.34H13.34" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M64.34 41.9565H57.34" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M0.339966 46.9565H64.34" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M0.339966 11.9565H64.34" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M64.34 51.9565H0.339966" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M64.34 7.8166L47.81 1.7966C46.33 1.2566 44.7 1.2566 43.22 1.7966L32.35 5.7566" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M38.01 7.8166L21.47 1.7966C19.99 1.2566 18.36 1.2566 16.88 1.7966L0.339966 7.8166" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M10.34 36.3164V42.9564" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
       <path
         d="M10.34 36.4765C8.02997 37.5665 6.48997 37.9065 3.33997 37.9065C5.42997 36.1965 6.07997 35.2265 6.83997 32.6765C5.67997 32.9665 4.58997 33.0065 3.33997 32.7465C6.83997 29.8365 8.11997 28.6365 10.34 23.9565C12.14 28.6065 13.52 29.8365 17.34 32.7465C15.89 33.0565 14.99 32.9465 13.84 32.6765C14.27 34.8665 15.35 36.3965 17.34 37.9065H17.32C14.27 37.7965 12.35 37.5165 10.34 36.4765Z"
         stroke="#0A622D"
-        stroke-width="2"
+        strokeWidth="2"
         stroke-miterlimit="10"
       />
-      <path d="M54.34 36.3164V42.9564" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
+      <path d="M54.34 36.3164V42.9564" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
       <path
         d="M54.34 36.4765C52.03 37.5665 50.49 37.9065 47.34 37.9065C49.43 36.1965 50.08 35.2265 50.84 32.6765C49.68 32.9665 48.59 33.0065 47.34 32.7465C50.84 29.8365 52.12 28.6365 54.34 23.9565C56.14 28.6065 57.52 29.8365 61.34 32.7465C59.89 33.0565 58.99 32.9465 57.84 32.6765C58.27 34.8665 59.35 36.3965 61.34 37.9065H61.32C58.27 37.7965 56.35 37.5165 54.34 36.4765Z"
         stroke="#0A622D"
-        stroke-width="2"
+        strokeWidth="2"
         stroke-miterlimit="10"
       />
     </g>
--- src/icons/SearchLandingIcon.tsx
+++ src/icons/SearchLandingIcon.tsx
@@ -4,17 +4,17 @@ const SearchLandingIcon = () => (
       <path
         d="M26.5 52.3965C40.5833 52.3965 52 40.9797 52 26.8965C52 12.8132 40.5833 1.39648 26.5 1.39648C12.4167 1.39648 1 12.8132 1 26.8965C1 40.9797 12.4167 52.3965 26.5 52.3965Z"
         stroke="#0A622D"
-        stroke-width="2"
+        strokeWidth="2"
         stroke-miterlimit="10"
       />
-      <path d="M48 41.3965L63 56.3965L56 63.3965L41 48.3965" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M13 44.3965H40" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M34 22.3965H19V38.3965H34V22.3965Z" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M19 6.39648V16.3965H34V6.39648" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M13 9.39648V16.3965H7" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M5 22.3965H13V38.3965H8" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M48 22.3965H40V38.3965H45" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
-      <path d="M40 9.39648V16.3965H45" stroke="#0A622D" stroke-width="2" stroke-miterlimit="10" />
+      <path d="M48 41.3965L63 56.3965L56 63.3965L41 48.3965" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M13 44.3965H40" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M34 22.3965H19V38.3965H34V22.3965Z" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M19 6.39648V16.3965H34V6.39648" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M13 9.39648V16.3965H7" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M5 22.3965H13V38.3965H8" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M48 22.3965H40V38.3965H45" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
+      <path d="M40 9.39648V16.3965H45" stroke="#0A622D" strokeWidth="2" stroke-miterlimit="10" />
     </g>
     <defs>
       <clipPath id="clip0_158_1441">
