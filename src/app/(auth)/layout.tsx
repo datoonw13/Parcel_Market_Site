99939ca5bafd20af024c2254236207d822bd3ff2
LGabelashvili <gabelashvili1999@gmail.com>
2024-03-25 23:05:45 +0400

    refactor code

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -8,7 +8,13 @@ const layout = ({
 }>) => (
   <div>
     <AuthHeader />
-    {children}
+    <div className="px-4 py-8 md:pb-12 lg:pb-16 xl:pb-18 2xl:pb-20 flex flex-col gap-10 max-w-[430px] m-auto">
+      <div className="w-[80%] sm:w-[100%] m-auto">
+        <h1 className="text-2xl font-bold font-bricolage text-green-900 text-center mb-6">Sign in with your email</h1>
+        <p className="text-xl text-dark-green-500 text-center font-medium">Enter the email address associated with your account.</p>
+      </div>
+      {children}
+    </div>
   </div>
 );
 
--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -4,11 +4,7 @@ import TextField from "@/components/shared/TextField";
 import GoogleIcon from "@/icons/GoogleIcon";
 
 const SignIn = () => (
-  <div className="px-4 py-8 md:pb-12 lg:pb-16 xl:pb-18 2xl:pb-20 flex flex-col gap-10 max-w-[430px] m-auto">
-    <div className="w-[80%] sm:w-[100%] m-auto">
-      <h1 className="text-2xl font-bold font-bricolage text-green-900 text-center mb-6">Sign in with your email</h1>
-      <p className="text-xl text-dark-green-500 text-center font-medium">Enter the email address associated with your account.</p>
-    </div>
+  <>
     <TextField label="Email" placeholder="Enter your email" />
     <Button>Continue</Button>
     <Divider label="OR" />
@@ -20,7 +16,7 @@ const SignIn = () => (
       <p>Don’t have an account yet?</p>
       <Button type="tertiary">Get Started</Button>
     </div>
-  </div>
+  </>
 );
 
 export default SignIn;
