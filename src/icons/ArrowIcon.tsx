24ed57977a6d92719541242f8c39ff429f7b8f92
LGabelashvili <gabelashvili1999@gmail.com>
2024-04-01 13:25:15 +0400

    fix styles

---
+++ src/icons/ArrowIcon.tsx
@@ -0,0 +1,7 @@
+const ArrowIcon = () => (
+  <svg viewBox="0 0 37 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M0.557068 0.636353L18.5 18.6414L36.4429 0.636353H0.557068Z" fill="#05471C" />
+  </svg>
+);
+
+export default ArrowIcon;
