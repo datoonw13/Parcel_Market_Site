aee4417a3ba1f4a545161aea68bedde0738808d5
LGabelashvili <gabelashvili1999@gmail.com>
2024-04-05 16:22:54 +0400

    fix map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+/* eslint-disable no-plusplus */
+/* eslint-disable no-param-reassign */
+/* eslint-disable global-require */
+
 // START: Preserve spaces to avoid auto-sorting
 import "leaflet/dist/leaflet.css";
 
@@ -18,26 +22,56 @@ interface IData {
 
 const data = require("../../public/test.json") as IMap;
 
-console.log(data);
+function reverse(arr: any) {
+  let leftIndex = 0;
+  let rightIndex = arr.length - 1;
+  while (leftIndex <= rightIndex) {
+    const isMiddle = leftIndex === rightIndex;
+    if (Array.isArray(arr[leftIndex])) {
+      reverse(arr[leftIndex]);
+    }
+    // do the same thing for the right side
+    // except for in case it's the "middle" value,
+    // in which case we would be un-sorting it by
+    // doing it twice
+    if (Array.isArray(arr[rightIndex]) && !isMiddle) {
+      reverse(arr[rightIndex]);
+    }
+
+    const temp = arr[leftIndex];
+    arr[leftIndex] = arr[rightIndex];
+    arr[rightIndex] = temp;
+    leftIndex++;
+    rightIndex--;
+  }
+
+  return arr;
+}
+
+const results = data.results.map((item) => item.geometry.coordinates);
+const buildings = reverse(data.buildings.map((item) => item.geometry.coordinates));
+const zoning = reverse(data.zoning.map((item) => item.geometry.coordinates));
 
 const Map = () =>
   data && (
     <MapContainer preferCanvas center={[39.817358, -86.251818]} zoom={600} scrollWheelZoom style={{ height: "800px", width: "1600px" }}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       <FeatureGroup pathOptions={{ color: "green" }}>
-        {/* {data?.zoning?.map((el) => (
-          <Polygon key={Math.random()} fillColor="red" positions={el.geometry.coordinates.map((item) => item.reverse()) as any} />
-        ))}
-        {data?.buildings?.map((el) => (
-          <Polygon key={Math.random()} fillColor="red" positions={el.geometry.coordinates.map((item) => item.reverse()) as any} />
-        ))} */}
-        {data?.results?.map((el) => (
+        {buildings.map((x: any) => (
           <>
-            <Polygon stroke key={Math.random()} fillColor="red" positions={el.geometry.coordinates as any} />
-            <Marker position={el.geometry.coordinates[0][0]} />
+            <Polygon stroke key={Math.random()} fillColor="blue" positions={x} />
+            <Marker position={x[0][0]} />
+          </>
+        ))}
+        {results.map((x) => (
+          <>
+            <Polygon stroke key={Math.random()} fillColor="purple" positions={x as any} />
+            {/* <Marker position={x[0][0]} /> */}
+          </>
+        ))}
+        {zoning.map((x: any) => (
+          <>
+            <Polygon stroke key={Math.random()} fillColor="red" positions={x} />
           </>
         ))}
       </FeatureGroup>
