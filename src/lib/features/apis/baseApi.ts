66b3e41d66dd51f63a5adb830d4ecc7c3380ab13
LGabelashvili <gabelashvili1999@gmail.com>
2024-04-08 12:58:56 +0400

    fix base api

--- src/lib/features/apis/baseApi.ts
+++ src/lib/features/apis/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import toast from "react-hot-toast";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: `http://64.23.229.149/api/`,
+  baseUrl: `http://64.23.229.149:4000/api/`,
   prepareHeaders: async (headers) => {
     headers.set("authorization", `Bearer ${localStorage.getItem("token")}`);
 
