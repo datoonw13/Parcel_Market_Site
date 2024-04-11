ac8f9d45e5b1793860c7c5e4e79890eeb1f1b537
LGabelashvili <gabelashvili1999@gmail.com>
2024-04-12 00:08:51 +0400

    remove unused files

--- src/lib/reducers.ts
+++ src/lib/reducers.ts
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
 import baseApi from "./features/apis/baseApi";
-import findProperty from "./features/slices/findPropertySlice";
 import authedUser from "./features/slices/authedUserSlice";
 
 export default combineReducers({
   [baseApi.reducerPath]: baseApi.reducer,
-  findProperty,
   authedUser,
 });
--- src/validations/property-schemas.ts
+++ src/validations/property-schemas.ts
@@ -1,36 +1,5 @@
 import * as yup from "yup";
 
-export const findPropertyInfoSchema = yup.object().shape(
-  {
-    owner: yup
-      .string()
-      .required("Owner name or parcel number is a required field")
-      .nullable()
-      .when("parcelNumber", ([parcelNumber], schema) =>
-        parcelNumber ? schema : schema.notOneOf([null], "Owner name or parcel number is a required field")
-      ),
-    state: yup.string().required("State is a required field").nullable().notOneOf([null], "State is a required field"),
-    county: yup.string().required("County is a required field").nullable().notOneOf([null], "County is a required field"),
-    parcelNumber: yup
-      .string()
-      .required("Parcel number or owner name is a required field")
-      .nullable()
-      .when("owner", ([owner], schema) => (owner ? schema : schema.notOneOf([null], "Parcel number or owner name is a required field"))),
-  },
-  [["owner", "parcelNumber"]]
-);
-
-export const findPropertyAboutSchema = yup.object().shape({
-  waterFeature: yup.boolean().required(),
-  waterFront: yup.boolean().required(),
-  langCoverType: yup.string().required(),
-  propertyCondition: yup.string().required(),
-  wetProperty: yup.string().required(),
-  propertyRestriction: yup.string().required(),
-  propertyAccess: yup.string().required(),
-  improvementsValue: yup.number().required(),
-});
-
 export const searchPropertySchema = yup.object().shape({
   info: yup.object().shape(
     {
