179eee59f1a2805283d5aea3e0daf659adfa853f
LGabelashvili <gabelashvili1999@gmail.com>
2024-04-12 17:21:33 +0400

    fx

--- src/lib/features/apis/baseApi.ts
+++ src/lib/features/apis/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import toast from "react-hot-toast";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: `http://64.23.229.149:4000/api/`,
+  baseUrl: `https://api.parcelmarket.com/api/`,
   prepareHeaders: async (headers) => {
     headers.set("authorization", `Bearer ${localStorage.getItem("token")}`);
 
