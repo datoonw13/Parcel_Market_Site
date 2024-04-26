f26793350f40932705e7a7095d7073d859239446
LGabelashvili <gabelashvili1999@gmail.com>
2024-04-26 12:26:00 +0400

    update styles

--- src/icons/LoadingCircle.tsx
+++ src/icons/LoadingCircle.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LoadingCircle = () => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
-    <circle fill="none" strokeOpacity="1" stroke="#16DB65" strokeWidth=".5" cx="100" cy="100" r="0">
+    <circle fill="none" strokeOpacity="1" stroke="#16DB65" strokeWidth="10" cx="100" cy="100" r="0">
       <animate attributeName="r" calcMode="spline" dur="2" values="1;80" keyTimes="0;1" keySplines="0 .2 .5 1" repeatCount="indefinite" />
       <animate
         attributeName="strokeWidth"
