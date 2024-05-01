b32bcc07308c2d1eb192424c70649b472f39dfca
LGabelashvili <gabelashvili1999@gmail.com>
2024-05-01 16:10:23 +0400

    fx

--- src/components/user/UserPropertyBox.tsx
+++ src/components/user/UserPropertyBox.tsx
@@ -20,40 +20,40 @@ const UserPropertyBox = () => (
         </h4>
       </div>
     </div>
-    <div className="flex flex-col md:flex-row md:justify-between gap-6 md:gap-8">
-      <div className="w-full h-[230px] md:h-auto md:w-[350px] lg:min-w-[350px] relative">
+    <div className="flex flex-col md:justify-between gap-6 md:gap-8">
+      <div className="w-full h-[230px] md:h-[340px] md:w-full lg:min-w-[350px] relative">
         <Image src="/no-parcel-image.png" alt="" fill />
-        <div className="absolute bg-[#363636] top-[24px] left-[24px] bg-opacity-85 z-10 rounded-3xl py-1 px-4 text-white font-medium text-sm flex items-center gap-2">
+        <div className="absolute bg-[#363636] top-[24px] left-[24px] bg-opacity-85 z-10 py-1 px-4 text-white font-medium text-sm flex items-center gap-2">
           <Eye />
           1245 Views
         </div>
       </div>
       <div className="flex flex-col gap-4 md:w-full">
-        <h2 className="text-lg font-medium text-[#363636]">General Information</h2>
-        <div className="bg-[#363636] bg-opacity-5 rounded-2xl p-4 grid grid-cols-2 md:grid-cols-2 gap-4">
-          <div className="flex flex-col gap-px">
-            <p className="text-xs md:text-sm text-[#545454]">Acrages:</p>
-            <p className="text-sm md:text-base text-[#363636] font-medium">45,900 Acres</p>
+        <h2 className="text-lg font-medium text-[#363636] md:hidden">General Information</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="flex gap-2 items-center">
+            <p className="text-sm md:text-base text-[#868686] font-semibold">Acrages:</p>
+            <p className="text-sm md:text-base text-[#363636] font-extrabold">45,900 Acres</p>
           </div>
-          <div className="flex flex-col gap-px">
-            <p className="text-xs md:text-sm text-[#545454]">Owner:</p>
-            <p className="text-sm md:text-base text-[#363636] font-medium">Davit Natelashvili</p>
+          <div className="flex gap-2 items-center">
+            <p className="text-sm md:text-base text-[#868686] font-semibold">Owner:</p>
+            <p className="text-sm md:text-base text-[#363636] font-extrabold">Davit Natelashvili</p>
           </div>
-          <div className="flex flex-col gap-px">
-            <p className="text-xs md:text-sm text-[#545454]">Parcel ID:</p>
-            <p className="text-sm md:text-base text-[#363636] font-medium">1473434052</p>
+          <div className="flex gap-2 items-center">
+            <p className="text-sm md:text-base text-[#868686] font-semibold">Parcel ID:</p>
+            <p className="text-sm md:text-base text-[#363636] font-extrabold">1473434052</p>
           </div>
-          <div className="flex flex-col gap-px">
-            <p className="text-xs md:text-sm text-[#545454]">Property Type</p>
-            <p className="text-sm md:text-base text-[#363636] font-medium">Property Type</p>
+          <div className="flex gap-2 items-center">
+            <p className="text-sm md:text-base text-[#868686] font-semibold">Property Type</p>
+            <p className="text-sm md:text-base text-[#363636] font-extrabold">Property Type</p>
           </div>
-          <div className="flex flex-col gap-px">
-            <p className="text-xs md:text-sm text-[#545454]">Average market price:</p>
-            <p className="text-sm md:text-base text-[#363636] font-medium">45,000,000 $</p>
+          <div className="flex gap-2 items-center">
+            <p className="text-sm md:text-base text-[#868686] font-semibold">Average market price:</p>
+            <p className="text-sm md:text-base text-[#363636] font-extrabold">45,000,000 $</p>
           </div>
-          <div className="flex flex-col gap-px">
-            <p className="text-xs md:text-sm text-[#545454]">Requested sales price:</p>
-            <p className="text-sm md:text-base text-[#363636] font-medium">43, 000,000 $</p>
+          <div className="flex gap-2 items-center">
+            <p className="text-sm md:text-base text-[#868686] font-semibold">Requested sales price:</p>
+            <p className="text-sm md:text-base text-[#363636] font-extrabold">43, 000,000 $</p>
           </div>
         </div>
       </div>
