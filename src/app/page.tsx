66df1341d1a1435c0ab2656911bbb22f54ba67d6
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-05-22 23:08:28 +0400

    fx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import Header from "@/components/header/Header";
-import Footer from "@/components/Footer";
 import LandingMain from "@/components/landing/LandingMain";
-import Container from "@/components/shared/Container";
-import { Box, Button, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import LandingSection1 from "@/components/landing/LandingSection1";
 
 const Landing = () => (
