2e446b50d4974be67c70043f248cf83db8fb573a
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-05-23 02:30:35 +0400

    fx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,20 @@ import dynamic from "next/dynamic";
 const LandingSection3 = dynamic(() => import("../components/landing/LandingSection3"), { ssr: false });
 
 const Landing = () => (
-  <Box sx={{ display: "flex", flexDirection: "column", gap: { xs: 10, md: 11, lg: 12.5 } }}>
-    <Box sx={{ bgcolor: "white" }}>
+  <Box sx={{ display: "flex", flexDirection: "column" }}>
+    <Box sx={{ bgcolor: "white", py: { xs: 10, md: 11, lg: 12.5 } }}>
       <Header />
       <LandingMain />
     </Box>
-    <LandingSection1 />
-    <LandingSection2 />
-    <LandingSection3 />
+    <Box sx={{ bgcolor: "white", py: { xs: 10, md: 11, lg: 12.5 } }}>
+      <LandingSection1 />
+    </Box>
+    <Box sx={{ bgcolor: "white", py: { xs: 10, md: 11, lg: 12.5 } }}>
+      <LandingSection2 />
+    </Box>
+    <Box sx={{ bgcolor: "grey.A100", py: { xs: 10, md: 11, lg: 12.5 } }}>
+      <LandingSection3 />
+    </Box>
     <div />
   </Box>
 );
