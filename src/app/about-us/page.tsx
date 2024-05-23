b23c7a5d04e477131a12012df50571d6b37def00
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-05-23 06:52:58 +0400

    add typos

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -8,54 +8,71 @@ const AboutUs = () => {
   const theme = useTheme();
   const upMd = useMediaQuery(theme.breakpoints.up("md"));
   return (
-    <Container>
-      <Box sx={{ py: { xs: 10, md: 11, lg: 12.5 }, display: "flex", flexDirection: "column", gap: 4.5 }}>
-        <Box sx={{ display: "flex", flexDirection: "column" }}>
-          <Typography sx={{ fontSize: { xs: 24, sm: 28, md: 32, lg: 36 }, textAlign: "center", fontWeight: 600 }}>About us</Typography>
-          <Typography sx={{ fontSize: 16, textAlign: "center", mt: { xs: 1.5, sm: 2, md: 3 }, fontWeight: { md: 500 } }}>
-            Welcome to Parcel Market and thank You for being here. At Parcel Market, our goal is simple, to provide a FREE and convenient
-            place to start when making decisions regarding vacant land.{" "}
-          </Typography>
-        </Box>
-        <Box sx={{ position: "relative" }}>
-          <Box
-            sx={{
-              height: {
-                xs: 240,
-                sm: 340,
-                md: 440,
-                lg: 530,
-                position: "relative",
-                "& > img": {
-                  borderTopLeftRadius: 16,
-                  borderTopRightRadius: 16,
-                  borderBottomLeftRadius: upMd ? 16 : 0,
-                  borderBottomRightRadius: upMd ? 16 : 0,
-                },
+    <Container sx={{ py: { xs: 10, md: 11, lg: 12.5 }, display: "flex", flexDirection: "column", gap: 4.5 }}>
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <Typography sx={{ fontSize: { xs: 24, sm: 28, md: 32, lg: 36 }, textAlign: "center", fontWeight: 600 }}>About us</Typography>
+        <Typography sx={{ fontSize: 16, textAlign: "center", mt: { xs: 1.5, sm: 2, md: 3 }, fontWeight: { md: 500 } }}>
+          Welcome to Parcel Market and thank You for being here. At Parcel Market, our goal is simple, to provide a FREE and convenient
+          place to start when making decisions regarding vacant land.{" "}
+        </Typography>
+      </Box>
+      <Box sx={{ position: "relative" }}>
+        <Box
+          sx={{
+            height: {
+              xs: 240,
+              sm: 340,
+              md: 440,
+              lg: 530,
+              position: "relative",
+              "& > img": {
+                borderTopLeftRadius: 16,
+                borderTopRightRadius: 16,
+                borderBottomLeftRadius: upMd ? 16 : 0,
+                borderBottomRightRadius: upMd ? 16 : 0,
               },
-            }}
-          >
-            <Image alt="" src="/people.png" fill loading="eager" style={{ transform: "scaleX(-1)" }} />
-          </Box>
-          <Box
-            sx={{
-              px: 2.5,
-              py: 2,
-              p: { md: 4.5 },
-              bgcolor: "primary.200",
-              borderBottomLeftRadius: { xs: 16 },
-              borderBottomRightRadius: { xs: 16, md: 0 },
-              borderTopRightRadius: { md: 16 },
-              fontWeight: 500,
-              fontSize: { xs: 14, md: 16 },
-              position: { md: "absolute" },
-              bottom: 0,
-              width: { md: "55%" },
-            }}
-          >
-            “We strive to work closely together with our customers in solving their problems. We value honesty, integrity, and efficiency”
-          </Box>
+            },
+          }}
+        >
+          <Image alt="" src="/people.png" fill loading="eager" style={{ transform: "scaleX(-1)" }} />
         </Box>
+        <Box
+          sx={{
+            px: 2.5,
+            py: 2,
+            p: { md: 4.5 },
+            bgcolor: "primary.200",
+            borderBottomLeftRadius: { xs: 16 },
+            borderBottomRightRadius: { xs: 16, md: 0 },
+            borderTopRightRadius: { md: 16 },
+            fontWeight: 500,
+            fontSize: { xs: 14, md: 16 },
+            position: { md: "absolute" },
+            bottom: 0,
+            width: { md: "55%" },
+          }}
+        >
+          “We strive to work closely together with our customers in solving their problems. We value honesty, integrity, and efficiency”
+        </Box>
+      </Box>
+      <Typography sx={{ fontSize: 14, color: "grey.800" }}>
+        While the information provided by Parcel Market’s Value Of land Tool (VOLT) is the main driver behind our purpose, we also wanted to
+        help our users utilize that information. We understand that the land business can be intimidating and finding qualified buyers,
+        sellers, or industry professionals can be quite the endeavor! We decided that providing options for taking the next steps after
+        determining land value is also very important. We offer three unique options for our users, and which one is right for you will
+        depend on your situation. They are The Parcel Market Purchase Option, The Parcel Market Marketplace, and Our Preferred Land
+        Specialists Network. Let’s explain these a bit further so you are well informed on how Parcel Market can help your specific needs.
+      </Typography>
+      <Box>
+        <Typography sx={{ fontSize: 18, fontWeight: 700, mb: 1.5 }}>The Parcel Market Value of Land Tool</Typography>
+        <Typography sx={{ fontSize: 14, color: "grey.800" }}>
+          While the information provided by Parcel Market’s Value Of land Tool (VOLT) is the main driver behind our purpose, we also wanted
+          to help our users utilize that information. We understand that the land business can be intimidating and finding qualified buyers,
+          sellers, or industry professionals can be quite the endeavor! We decided that providing options for taking the next steps after
+          determining land value is also very important. We offer three unique options for our users, and which one is right for you will
+          depend on your situation. They are The Parcel Market Purchase Option, The Parcel Market Marketplace, and Our Preferred Land
+          Specialists Network. Let’s explain these a bit further so you are well informed on how Parcel Market can help your specific needs.
+        </Typography>
       </Box>
     </Container>
   );
