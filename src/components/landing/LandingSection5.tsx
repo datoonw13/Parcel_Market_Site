5f65e2994f0131040799aec7142c1f8574b6592f
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-05-23 05:01:42 +0400

    add section

--- src/components/landing/LandingSection5.tsx
+++ src/components/landing/LandingSection5.tsx
@@ -1,5 +1,7 @@
-import { Box, Container, Typography } from "@mui/material";
-import React from "react";
+"use client";
+
+import { Accordion, AccordionDetails, AccordionSummary, Box, Container, Typography } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const LandingSection5 = () => (
   <Container
@@ -17,19 +19,36 @@ const LandingSection5 = () => (
         Frequently asked questions
       </Typography>
     </Box>
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: { xs: "column", md: "row" },
-        gap: { xs: 2.5, sm: 3, md: 3.5, lg: 4.5 },
-        width: "100%",
-        alignItems: { xs: "center", md: "start" },
-        justifyContent: "center",
-      }}
-    >
-      ae
+    <Box sx={(theme) => ({ border: `1px solid ${theme.palette.grey[200]}`, p: { xs: 3, md: 4.5 }, borderRadius: 2 })}>
+      {qa.map((el) => (
+        <Accordion
+          key={el.title}
+          sx={(theme) => ({
+            boxShadow: "0",
+            borderRadius: "0 !important",
+            p: 0,
+            border: 0,
+            py: 1,
+          })}
+        >
+          <AccordionSummary
+            sx={{ p: 0, fontSize: 16, fontWeight: 500 }}
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+          >
+            {el.title}
+          </AccordionSummary>
+          <AccordionDetails sx={{ px: 0, fontSize: 14, color: "grey.600" }}>{el.desc}</AccordionDetails>
+        </Accordion>
+      ))}
     </Box>
   </Container>
 );
 
 export default LandingSection5;
+
+const qa = new Array(6).fill(0).map((_, i) => ({
+  title: `Frequently asked question ${i + 1}`,
+  desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+}));
