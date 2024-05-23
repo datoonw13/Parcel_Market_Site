fc7f033b58462bc68f91a352b883a65a2231ef4c
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-05-23 07:03:23 +0400

    fix typos

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -74,6 +74,128 @@ const AboutUs = () => {
           Specialists Network. Let’s explain these a bit further so you are well informed on how Parcel Market can help your specific needs.
         </Typography>
       </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: { xs: 3, sm: 4, md: 5, lg: 6 },
+        }}
+      >
+        <Typography
+          sx={{
+            fontWeight: 600,
+            fontSize: {
+              xs: 18,
+              sm: 22,
+              md: 24,
+              lg: 28,
+            },
+          }}
+        >
+          Our 3 unique options for users
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            gap: { xs: 1.5, md: 12 },
+            justifyContent: { md: "space-between" },
+            flexDirection: { xs: "column", md: "row" },
+            alignItems: { md: "start" },
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: { xs: "column", md: "row" },
+              justifyContent: "center",
+              alignItems: "center",
+              gap: 1,
+              color: "primary.main",
+            }}
+          >
+            <Typography sx={{ fontSize: 18, fontWeight: 500 }}>01.</Typography>
+            <Typography sx={{ fontSize: 18, fontWeight: 700, width: { md: "max-content" } }}>The Parcel Market Purchase Option</Typography>
+          </Box>
+          <Typography sx={{ fontSize: 14, color: "grey.800" }}>
+            For sellers who are looking to liquidate quickly and with no closing expenses, our Parcel Market Purchase Option may be the
+            right fit. Under this option, Parcel Market will make an offer to buy your land based on the VOLT value. If accepted, we will
+            handle everything from contracts to closing! You will also be able to close remotely, at your convenience. As an added benefit
+            and peace of mind, Parcel Market will pay a third-party attorney of your choice to review the purchase agreement on your behalf.
+            While our Parcel Market Purchase Option offers the fastest way to sell your land with no closing expenses and via a remote
+            closing, it&apos;s important to note that the offer will be below the VOLT value and therefore likely below the market value of
+            your property. This option is ideal for sellers looking for a quick and hassle-free sale, prioritizing speed and convenience
+            over maximizing sale price.
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            gap: { xs: 1.5, md: 12 },
+            justifyContent: { md: "space-between" },
+            flexDirection: { xs: "column", md: "row" },
+            alignItems: { md: "start" },
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: { xs: "column", md: "row" },
+              justifyContent: "center",
+              alignItems: "center",
+              gap: 1,
+              color: "primary.main",
+            }}
+          >
+            <Typography sx={{ fontSize: 18, fontWeight: 500 }}>01.</Typography>
+            <Typography sx={{ fontSize: 18, fontWeight: 700, width: { md: "max-content" } }}>The Parcel Market Purchase Option</Typography>
+          </Box>
+          <Typography sx={{ fontSize: 14, color: "grey.800" }}>
+            For sellers who are looking to liquidate quickly and with no closing expenses, our Parcel Market Purchase Option may be the
+            right fit. Under this option, Parcel Market will make an offer to buy your land based on the VOLT value. If accepted, we will
+            handle everything from contracts to closing! You will also be able to close remotely, at your convenience. As an added benefit
+            and peace of mind, Parcel Market will pay a third-party attorney of your choice to review the purchase agreement on your behalf.
+            While our Parcel Market Purchase Option offers the fastest way to sell your land with no closing expenses and via a remote
+            closing, it&apos;s important to note that the offer will be below the VOLT value and therefore likely below the market value of
+            your property. This option is ideal for sellers looking for a quick and hassle-free sale, prioritizing speed and convenience
+            over maximizing sale price.
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            gap: { xs: 1.5, md: 12 },
+            justifyContent: { md: "space-between" },
+            flexDirection: { xs: "column", md: "row" },
+            alignItems: { md: "start" },
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: { xs: "column", md: "row" },
+              justifyContent: "center",
+              alignItems: "center",
+              gap: 1,
+              color: "primary.main",
+            }}
+          >
+            <Typography sx={{ fontSize: 18, fontWeight: 500 }}>01.</Typography>
+            <Typography sx={{ fontSize: 18, fontWeight: 700, width: { md: "max-content" } }}>The Parcel Market Purchase Option</Typography>
+          </Box>
+          <Typography sx={{ fontSize: 14, color: "grey.800" }}>
+            For sellers who are looking to liquidate quickly and with no closing expenses, our Parcel Market Purchase Option may be the
+            right fit. Under this option, Parcel Market will make an offer to buy your land based on the VOLT value. If accepted, we will
+            handle everything from contracts to closing! You will also be able to close remotely, at your convenience. As an added benefit
+            and peace of mind, Parcel Market will pay a third-party attorney of your choice to review the purchase agreement on your behalf.
+            While our Parcel Market Purchase Option offers the fastest way to sell your land with no closing expenses and via a remote
+            closing, it&apos;s important to note that the offer will be below the VOLT value and therefore likely below the market value of
+            your property. This option is ideal for sellers looking for a quick and hassle-free sale, prioritizing speed and convenience
+            over maximizing sale price.
+          </Typography>
+        </Box>
+      </Box>
     </Container>
   );
 };
