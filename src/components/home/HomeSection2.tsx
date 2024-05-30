7d8ee694911c8c68d23f67bc8bc9d9e90a6e7afc
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-05-31 00:42:12 +0400

    add links

--- src/components/home/HomeSection2.tsx
+++ src/components/home/HomeSection2.tsx
@@ -4,6 +4,7 @@ import ConnectIcon from "@/icons/ConnectIcon";
 import DiscoverIcon from "@/icons/DiscoverIcon";
 import SellingIcon from "@/icons/SellingIcon";
 import { Box, Button, Container, Typography } from "@mui/material";
+import Link from "next/link";
 
 const HomeSection2 = () => (
   <Container
@@ -38,27 +39,28 @@ const HomeSection2 = () => (
       }}
     >
       {list.map((el) => (
-        <Box
-          key={el.title}
-          sx={(theme) => ({
-            border: `1px solid ${theme.palette.grey[200]}`,
-            p: { xs: 3, sm: 3.5, md: 4 },
-            borderRadius: 4,
-            height: { xs: 400, sm: 460, lg: 424 },
-            width: "100%",
-            maxWidth: 350,
-            cursor: "pointer",
-            "&:hover": {
-              boxShadow: theme.shadows[1],
-            },
-          })}
-        >
-          <Box sx={{ mb: 4.5, "& > svg": { width: 142, height: 140 } }}>
-            <el.icon />
+        <Link key={el.title} href={el.path}>
+          <Box
+            sx={(theme) => ({
+              border: `1px solid ${theme.palette.grey[200]}`,
+              p: { xs: 3, sm: 3.5, md: 4 },
+              borderRadius: 4,
+              height: { xs: 400, sm: 460, lg: 424 },
+              width: "100%",
+              maxWidth: 350,
+              cursor: "pointer",
+              "&:hover": {
+                boxShadow: theme.shadows[1],
+              },
+            })}
+          >
+            <Box sx={{ mb: 4.5, "& > svg": { width: 142, height: 140 } }}>
+              <el.icon />
+            </Box>
+            <Typography sx={{ fontWeight: 700, fontSize: 18, mb: 1 }} dangerouslySetInnerHTML={{ __html: el.title }} />
+            <Typography sx={{ color: "grey.800" }}>{el.desc}</Typography>
           </Box>
-          <Typography sx={{ fontWeight: 700, fontSize: 18, mb: 1 }} dangerouslySetInnerHTML={{ __html: el.title }} />
-          <Typography sx={{ color: "grey.800" }}>{el.desc}</Typography>
-        </Box>
+        </Link>
       ))}
     </Box>
   </Container>
@@ -71,15 +73,18 @@ const list = [
     title: "Sell Quickly With No <br/> Closing Expenses",
     desc: "Get quick cash for your property, with zero closing costs and zero hassle with our Parcel Market Purchase Option.",
     icon: SellingIcon,
+    path: "/sell-quickly",
   },
   {
     title: "Discover Your next <br/> Land Deal",
     desc: "Utilize The Parcel Market Marketplace to discover new land deals or expose your property to investors.",
     icon: DiscoverIcon,
+    path: "/discover-deal",
   },
   {
     title: "Connect with a <br/> Preferred Professional",
     desc: "Connect with an experienced land professional in your area using Parcel Market’s Preferred Professional Network",
     icon: ConnectIcon,
+    path: "/connect-professional",
   },
 ];
