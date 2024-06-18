fa9c1f203f26961b219fd3616ca1631eaf76a074
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-06-19 00:24:42 +0400

    refactor map

--- src/components/find-property/FindPropertyFoundedParcels.tsx
+++ src/components/find-property/FindPropertyFoundedParcels.tsx
@@ -6,7 +6,7 @@ import { Box, Button, Divider, Typography } from "@mui/material";
 import dynamic from "next/dynamic";
 import React, { Dispatch, SetStateAction, useEffect } from "react";
 
-const Map = dynamic(() => import("@/components/find-property/Map"), { ssr: false });
+const Map = dynamic(() => import("@/components/shared/Map"), { ssr: false });
 
 interface IProps {
   data: IMap;
@@ -66,9 +66,27 @@ const FindPropertyFoundedParcels = ({ data, selectedRegridItem, setSelectedRegri
           <Box sx={{ borderRadius: 4, height: { xs: 240, sm: 230, md: 220, lg: 200 }, "& > div": { borderRadius: 4 } }}>
             {data.length > 0 && (
               <Map
-                data={data}
+                data={data.map((el) => ({
+                  centerCoordinate: [Number(el.properties.fields.lat), Number(el.properties.fields.lon)],
+                  polygon: el.geometry.coordinates,
+                  owner: el.properties.fields.owner,
+                  parcelNumber: el.properties.fields.parcelnumb,
+                  showMarker: true,
+                  markerColor: "default",
+                  popup: {
+                    owner: el.properties.fields.parcelnumb,
+                    parcelNumber: el.properties.fields.owner,
+                    showSelectButton: data.length > 1,
+                  },
+                }))}
                 selectedParcelNumber={selectedRegridItem?.properties.fields.parcelnumb || ""}
-                handleParcelSelect={(val) => setSelectedRegridItem(val)}
+                onSelect={(parcelNumber) => {
+                  const item = data.find((el) => el.properties.fields.parcelnumb === parcelNumber);
+                  setSelectedRegridItem(item || null);
+                }}
+                onDiscard={() => setSelectedRegridItem(null)}
+                zoom={5}
+                geolibInputCoordinates={data.map((el) => [Number(el.properties.fields.lat), Number(el.properties.fields.lon)])}
               />
             )}
           </Box>
@@ -148,3 +166,82 @@ const FindPropertyFoundedParcels = ({ data, selectedRegridItem, setSelectedRegri
 };
 
 export default FindPropertyFoundedParcels;
+
+// "use client";
+
+// import FbIcon from "@/icons/FbIcon";
+// import TwitterIcon from "@/icons/TwitterIcon";
+// import { Box, Typography, alpha } from "@mui/material";
+// import Image from "next/image";
+// import React, { useState } from "react";
+// import { IFindPropertyEstimatedPriceResponse } from "@/types/find-property";
+// import dynamic from "next/dynamic";
+// import { IMap, IMapItem } from "@/types/map";
+// import FindProperty from "@/components/find-property/FindProperty";
+// import { MiniFooter } from "@/components/footer";
+// import { AppBarMini } from "@/components/app-bar";
+// import useAuthCheck from "@/hooks/useAuthCheck";
+// import { LatLngTuple } from "leaflet";
+// import { PolygonProps } from "react-leaflet";
+// import { getCenter } from "geolib";
+
+// const FindPropertyCalculatePriceMap = dynamic(() => import("@/components/find-property/FindPropertyCalculatePriceMap"), {
+//   ssr: false,
+// });
+// const Map = dynamic(() => import("../../components/shared/Map"), { ssr: false });
+
+// interface IProps {
+//   mapCenter: LatLngTuple;
+//   zoom: number;
+//   data: Array<{
+//     centerCoordinate: LatLngTuple;
+//     polygon?: PolygonProps["positions"];
+//     owner: string | null;
+//     parcelNumber: string;
+//     showMarker?: boolean;
+//     markerColor: "default" | "red" | "green";
+//     popup?: {
+//       owner?: string;
+//       parcelNumber?: string;
+//       selectable?: boolean;
+//     };
+//   }>;
+//   selectedParcelNumber?: string | null;
+//   onSelect?: (parcelNumber: string) => void;
+// }
+
+// const res =
+//   '[{"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[32.8345255,-80.69059],[32.8346335,-80.689918],[32.834704,-80.689919],[32.835211,-80.6899235],[32.8351025,-80.690595],[32.8345255,-80.69059]]]},"properties":{"headline":"95 Cornwall Ln","path":"/us/sc/colleton/hendersonville/30357","fields":{"ogc_fid":30357,"geoid":"45029","parcelnumb":"208-00-00-031.000","parcelnumb_no_formatting":"2080000031000","usedesc":"VACANT RESIDENTIAL","zoning":"UD1","zoning_description":"Urban Development 1","owner":"HICKMAN CHRISTOPHER D","mailadd":"168 J RUFUS RD","mail_city":"LEXINGTON","mail_state2":"SC","mail_zip":"29073-7419","address":"95 CORNWALL LN","saddno":"95","saddstr":"CORNWALL","saddsttyp":"LN","scity":"WALTERBORO","original_address":"{\\"address\\":\\"95 CORNWALL LANE \\"}","city":"hendersonville","county":"colleton","state2":"SC","szip":"29488-8997","szip5":"29488","address_source":"county;cass","lat":"32.834868","lon":"-80.690257","qoz":"No","census_tract":"45029970302","census_block":"450299703021004","census_blockgroup":"450299703021","census_zcta":"29488","ll_last_refresh":"2024-05-21","gisacre":1,"ll_gisacre":0.99535,"ll_gissqft":43358,"path":"/us/sc/colleton/hendersonville/30357","ll_stable_id":"parcelnumb","ll_uuid":"e0dcfddf-398e-4f48-8ff5-b5e8440bfe83","ll_updated_at":"2024-06-10 22:25:36 -0400"},"context":{"headline":"Hendersonville, SC","name":"Hendersonville, SC","path":"/us/sc/colleton/hendersonville","active":true},"ll_uuid":"e0dcfddf-398e-4f48-8ff5-b5e8440bfe83"},"id":122713872},{"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[33.043988,-80.442375],[33.04378,-80.4412055],[33.0454205,-80.439827],[33.045673,-80.4412105],[33.0453775,-80.4414585],[33.045034,-80.4417465],[33.044697,-80.442029],[33.043988,-80.442375]]]},"properties":{"headline":"24349 Augusta Hwy","path":"/us/sc/colleton/cottageville/5193","fields":{"ogc_fid":5193,"geoid":"45029","parcelnumb":"067-00-00-117.000","parcelnumb_no_formatting":"0670000117000","usedesc":"IMPROVED AGRICULTURAL","zoning":"RD2","zoning_description":"Rural Development 2","owner":"HICKMAN CHRISTOPHER","mailadd":"24349 AUGUSTA HWY","mail_city":"COTTAGEVILLE","mail_state2":"SC","mail_zip":"29435","address":"24349 AUGUSTA HWY","saddno":"24349","saddstr":"AUGUSTA","saddsttyp":"HWY","scity":"COTTAGEVILLE","original_address":"{\\"address\\":\\"24349 AUGUSTA HWY \\",\\"scity\\":\\"COTTAGEVILLE \\",\\"szip\\":\\"29435 \\"}","city":"cottageville","county":"colleton","state2":"SC","szip":"29435-3933","szip5":"29435","address_source":"county;cass","lat":"33.044741","lon":"-80.441172","qoz":"No","census_tract":"45029970702","census_block":"450299707022005","census_blockgroup":"450299707022","census_zcta":"29435","ll_last_refresh":"2024-05-21","gisacre":6.5,"ll_gisacre":6.4885,"ll_gissqft":282645,"path":"/us/sc/colleton/cottageville/5193","ll_stable_id":"parcelnumb","ll_uuid":"6d85b8ea-86de-4254-84b0-263c013d8156","ll_updated_at":"2024-06-10 22:25:36 -0400"},"context":{"headline":"Cottageville, SC","name":"Cottageville, SC","path":"/us/sc/colleton/cottageville","active":true},"ll_uuid":"6d85b8ea-86de-4254-84b0-263c013d8156"},"id":122728474}]';
+// const data = (JSON.parse(res) as IMap).map((el) => ({
+//   centerCoordinate: [Number(el.properties.fields.lat), Number(el.properties.fields.lon)],
+//   polygon: el.geometry.coordinates,
+//   owner: el.properties.fields.owner,
+//   parcelNumber: el.properties.fields.parcelnumb,
+//   showMarker: true,
+//   markerColor: "default",
+//   popup: {
+//     owner: el.properties.fields.parcelnumb,
+//     parcelNumber: el.properties.fields.owner,
+//     selectable: true,
+//   },
+// })) as IProps["data"];
+
+// const mapCenter = getCenter(data.map((el) => ({ latitude: el.centerCoordinate[0], lon: el.centerCoordinate[1] })));
+
+// const FindPropertyLayout = () => {
+//   const [calculatedPrice, setCalculatedPrice] = useState<IFindPropertyEstimatedPriceResponse | null>(null);
+//   const [selectedRegridItem, setSelectedRegridItem] = useState<IMapItem | null>(null);
+//   useAuthCheck();
+//   return (
+//     <Box sx={{ width: "100%", height: 500 }}>
+//       <Map
+//         data={data}
+//         mapCenter={mapCenter ? [mapCenter.latitude, mapCenter.longitude] : [0, 0]}
+//         zoom={8}
+//         selectedParcelNumber="067-00-00-117.000"
+//       />
+//     </Box>
+//   );
+// };
+
+// export default FindPropertyLayout;
---
+++ src/components/shared/Map.tsx
@@ -0,0 +1,124 @@
+"use client";
+
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
+import "leaflet-defaulticon-compatibility";
+import { Button } from "@mui/material";
+import { LatLngTuple, Icon } from "leaflet";
+import { Fragment } from "react";
+import { FeatureGroup, MapContainer, Marker, Polygon, PolygonProps, Popup, TileLayer } from "react-leaflet";
+import { getCenter } from "geolib";
+
+interface IProps {
+  geolibInputCoordinates: Array<LatLngTuple>;
+  zoom: number;
+  data: Array<{
+    centerCoordinate: LatLngTuple;
+    polygon?: PolygonProps["positions"];
+    owner: string | null;
+    parcelNumber: string;
+    showMarker?: boolean;
+    markerColor: "default" | "red" | "green";
+    popup?: {
+      owner?: string;
+      parcelNumber?: string;
+      showSelectButton?: boolean;
+    };
+  }>;
+  selectedParcelNumber?: string | null;
+  onSelect?: (parcelNumber: string) => void;
+  onDiscard?: (parcelNumber: string) => void;
+}
+
+const markerGrey = new Icon({
+  iconUrl: "/marker-grey.svg",
+  iconSize: [35, 35],
+});
+
+const markerRed = new Icon({
+  iconUrl: "/marker-red.svg",
+  iconSize: [35, 35],
+});
+
+const markerGreen = new Icon({
+  iconUrl: "/marker-red.svg",
+  iconSize: [35, 35],
+});
+
+const getMarkerIcon = (mapItem: IProps["data"][0], selectedParcelNumber: IProps["selectedParcelNumber"]) => {
+  if (mapItem.parcelNumber === selectedParcelNumber) {
+    return markerRed;
+  }
+  if (mapItem.markerColor === "default") {
+    return markerGrey;
+  }
+  if (mapItem.markerColor === "red") {
+    return markerRed;
+  }
+  if (mapItem.markerColor === "green") {
+    return markerGreen;
+  }
+  return undefined;
+};
+
+const Map = ({ geolibInputCoordinates, data, zoom, selectedParcelNumber, onSelect, onDiscard }: IProps) => {
+  const mapCenter = getCenter(geolibInputCoordinates.map((el) => ({ latitude: el[0], lon: el[1] })));
+
+  return (
+    <MapContainer
+      zoom={zoom}
+      center={mapCenter ? [mapCenter.latitude, mapCenter.longitude] : [0, 0]}
+      scrollWheelZoom
+      style={{ height: "100%", width: "100%" }}
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <FeatureGroup pathOptions={{ color: "green" }}>
+        {data?.map((mapItem) => (
+          <Fragment key={mapItem.parcelNumber}>
+            {mapItem.polygon && <Polygon stroke key={Math.random()} fillColor="blue" positions={mapItem.polygon} />}
+            {mapItem.showMarker && (
+              <Marker icon={getMarkerIcon(mapItem, selectedParcelNumber)} position={mapItem.centerCoordinate}>
+                {mapItem.popup && (
+                  <Popup>
+                    <div className="flex flex-col gap-1">
+                      {mapItem.popup.owner && (
+                        <div>
+                          Owner: <b>{mapItem.owner}</b>
+                        </div>
+                      )}
+                      {mapItem.popup.parcelNumber && (
+                        <div>
+                          Parcel Number: <b>#{mapItem.parcelNumber}</b>
+                        </div>
+                      )}
+                    </div>
+                    {mapItem.popup.showSelectButton && (
+                      <Button
+                        color={selectedParcelNumber === mapItem.parcelNumber ? "error" : "primary"}
+                        variant="contained"
+                        size="small"
+                        sx={{ p: 1, my: 2, ml: "auto", display: "flex" }}
+                        onClick={() => {
+                          if (selectedParcelNumber === mapItem.parcelNumber && onDiscard) {
+                            onDiscard(mapItem.parcelNumber);
+                          }
+                          if (selectedParcelNumber !== mapItem.parcelNumber && onSelect) {
+                            onSelect(mapItem.parcelNumber);
+                          }
+                        }}
+                      >
+                        {selectedParcelNumber === mapItem.parcelNumber ? "Discard" : "Select"}
+                      </Button>
+                    )}
+                  </Popup>
+                )}
+              </Marker>
+            )}
+          </Fragment>
+        ))}
+      </FeatureGroup>
+    </MapContainer>
+  );
+};
+
+export default Map;
