4c3b85b94e5c149b908439ab7f84e29ff8d34fea
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-06-20 06:04:27 +0400

    fix bugs

--- src/components/@new/shared/forms/textfield/TextField.tsx
+++ src/components/@new/shared/forms/textfield/TextField.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import clsx from "clsx";
 import classes from "./textfield.module.css";
 
@@ -6,13 +8,20 @@ interface TextFieldProps {
   label?: string;
   placeholder?: string;
   error?: boolean;
+  value?: string;
+  onChange?: (value: string) => void;
 }
 
 const TextField = (props: TextFieldProps) => {
-  const { variant = "primary", label, placeholder = "", error } = props;
+  const { variant = "primary", label, placeholder = "", error, value, onChange } = props;
   return (
     <div className={clsx(classes.root)}>
-      <input className={clsx(label && classes[`input-${variant}`], classes.input, error && classes.error)} placeholder={placeholder} />
+      <input
+        className={clsx(label && classes[`input-${variant}`], classes.input, error && classes.error)}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => onChange && onChange(e.target.value)}
+      />
       {label && <p className={label && classes[`label-${variant}`]}>{label}</p>}
     </div>
   );
