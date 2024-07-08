dcc19ab7518815c172b59335abbae610a9500ad3
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-07-09 02:10:28 +0400

    refactor: refactor user offers pages

--- src/app/(main)/(authed)/user/received-offers/components/UserReceivedOffersFilters.tsx
+++ src/app/(main)/(authed)/user/received-offers/components/UserReceivedOffersFilters.tsx
@@ -6,7 +6,7 @@ import { getMinMaxFilterLabel, priceFilters } from "@/components/@new/shared/fil
 import { OfferStatus } from "@/types/offer";
 
 const UserReceivedOffersFilters = () => (
-  <div className="flex gap-3">
+  <div className="flex gap-3 mb-6">
     <AutoCompleteDesktopFilters filterKey="parcelNumber" placeholder="Parcel ID" options={[]} />
     <AutoCompleteDesktopFilters
       filterKey="status"
--- src/app/(main)/(authed)/user/received-offers/page.tsx
+++ src/app/(main)/(authed)/user/received-offers/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import OfferBox from "@/components/@new/offer/offer-box.tsx/OfferBox";
+import UserProfileSectionHeader from "@/components/@new/user/UserProfileSectionHeader";
 import UserReceivedOffersFilters from "./components/UserReceivedOffersFilters";
 
 const UserReceivedOffers = () => (
-  <div className="space-y-6">
+  <div className="">
+    <UserProfileSectionHeader title="Received Offers" description="View and assess the proposals you've received for your property." />
     <UserReceivedOffersFilters />
     <OfferBox />
   </div>
