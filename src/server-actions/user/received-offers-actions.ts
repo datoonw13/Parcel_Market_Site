00058d3d5e9c1aa7ba9db7cb6401ee155747518a
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-07-09 14:14:00 +0400

    feat: add tags for data cache

--- src/server-actions/user/received-offers-actions.ts
+++ src/server-actions/user/received-offers-actions.ts
@@ -3,10 +3,12 @@
 import { IPagination, ResponseType } from "@/types/common";
 import { ReceivedOffersFilters } from "@/types/offer";
 import { fetcher } from "../fetcher";
+import { userTags } from "./user-tags";
 
 export const getUserReceivedOffers = async (params: ReceivedOffersFilters): Promise<(ResponseType<any[]> & IPagination) | null> => {
   const request = await fetcher<ResponseType<(ResponseType<any[]> & IPagination) | null>>(
-    `offers/received?${new URLSearchParams(params as Record<string, string>)}`
+    `offers/received?${new URLSearchParams(params as Record<string, string>)}`,
+    { next: { tags: [userTags.receivedOffers] } }
   );
   return request.data?.data || null;
 };
---
+++ src/server-actions/user/user-tags.ts
@@ -0,0 +1,3 @@
+export const userTags = {
+  receivedOffers: "/user/receivedOffers",
+};
