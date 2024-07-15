d4d91670eef389cf066e13d9b62023faa6ba0629
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-07-15 06:40:15 +0400

    update api url

--- src/server-actions/fetcher.ts
+++ src/server-actions/fetcher.ts
@@ -5,7 +5,7 @@ import { cookies } from "next/headers";
 
 // eslint-disable-next-line no-undef
 export const fetcher = async <T>(url: string, options?: RequestInit): Promise<T> => {
-  const request = await fetch(`http://localhost:4000/api/${url}`, {
+  const request = await fetch(`https://api.parcelmarket.com/api/${url}`, {
     ...options,
     headers: {
       "Content-Type": "application/json",
