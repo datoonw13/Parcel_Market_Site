725e61746dccdb2d6b6e4776fc026199226f0003
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-07-15 06:29:38 +0400

    feat: accept offers

--- src/components/@new/user/offers/received/offer-details-wrapper.tsx
+++ src/components/@new/user/offers/received/offer-details-wrapper.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import Button from "@/components/@new/shared/forms/Button";
-import routes from "@/helpers/routes";
-import Link from "next/link";
 import { useState } from "react";
-import { OfferModel } from "@/types/offer";
+import { OfferModel, OfferStatusEnum } from "@/types/offer";
 import ResponsiveRemoveModal from "@/components/@new/shared/modals/ResponsiveRemoveModal";
 import clsx from "clsx";
+import { acceptReceivedOffer, rejectReceivedOffer } from "@/server-actions/offer/actions";
+import toast from "react-hot-toast";
+import { usePathname, useRouter } from "next/navigation";
+import ResponsiveAcceptModal from "@/components/@new/shared/modals/ResponsiveAcceptModal";
 import OfferDetail from "../details/offer-detail";
 
 const ReceivedOfferDetailsWrapper = ({
@@ -18,7 +20,33 @@ const ReceivedOfferDetailsWrapper = ({
   contentClassName?: string;
   actionClassName?: string;
 }) => {
-  const [openCancelModal, setOpenCancelModal] = useState(false);
+  const [pending, setPending] = useState(false);
+  const [openRejectModal, setOpenRejectModal] = useState(false);
+  const [openAcceptModal, setOpenAcceptModal] = useState(false);
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const rejectOffer = async () => {
+    setPending(true);
+    const { errorMessage } = await rejectReceivedOffer(data.id);
+    if (errorMessage) {
+      toast.error(errorMessage);
+      setPending(false);
+    } else {
+      router.replace(`${pathname}?reject=${data.id}`);
+    }
+  };
+
+  const acceptOffer = async () => {
+    setPending(true);
+    const { errorMessage } = await acceptReceivedOffer(data.id);
+    if (errorMessage) {
+      toast.error(errorMessage);
+      setPending(false);
+    } else {
+      router.replace(`${pathname}?accept=${data.id}`);
+    }
+  };
 
   return (
     <>
@@ -28,25 +56,44 @@ const ReceivedOfferDetailsWrapper = ({
         </div>
         <div className={clsx("flex gap-3 border-t border-t-grey-100", actionClassName)}>
           <Button variant="secondary">Contact Buyer</Button>
-          <Button color="error" className="ml-auto" onClick={() => setOpenCancelModal(true)}>
-            Reject
-          </Button>
-          <Button>Accept</Button>
+          {data.offerStatus === OfferStatusEnum.pending && (
+            <>
+              <Button color="error" className="ml-auto" onClick={() => setOpenRejectModal(true)}>
+                Decline
+              </Button>
+              <Button onClick={() => setOpenAcceptModal(true)}>Accept</Button>
+            </>
+          )}
         </div>
       </div>
-      {/* <ResponsiveRemoveModal
-        open={openCancelModal}
-        pending={false}
+      <ResponsiveRemoveModal
+        open={openRejectModal}
+        pending={pending}
         handleClose={() => {
-          setOpenCancelModal(false);
+          setOpenRejectModal(false);
         }}
         onReject={() => {
-          setOpenCancelModal(false);
+          setOpenRejectModal(false);
         }}
-        title="Cancel Offer??"
-        desc="Are you sure you want to cancel your offer?"
-        onOk={() => setOpenCancelModal(false)}
-      /> */}
+        title="Decline Offer?"
+        desc="Are you sure you want to decline this offer?"
+        onOk={rejectOffer}
+        acceptLabel="Decline"
+      />
+      <ResponsiveAcceptModal
+        open={openAcceptModal}
+        pending={pending}
+        handleClose={() => {
+          setOpenAcceptModal(false);
+        }}
+        onReject={() => {
+          setOpenAcceptModal(false);
+        }}
+        title="Accept Offer??"
+        desc="This offer is for initial contact only and is not a binding contract. Please accept to proceed."
+        onOk={acceptOffer}
+        acceptLabel="Accept"
+      />
     </>
   );
 };
--- src/server-actions/offer/actions.ts
+++ src/server-actions/offer/actions.ts
@@ -9,91 +9,6 @@ import { ErrorResponse } from "@/helpers/error-response";
 import { fetcher } from "../fetcher";
 import { offerTags } from "./tags";
 
-export const getReceivedOffersAction = async (
-  params: {[key: string]: string}
-): Promise<ResponseModel<({ list: OfferModel[] } & IPagination) | null>> => {
-  try {
-    const request = await fetcher<{ data: OfferModel[] } & IPagination>(
-      `offers/received?${new URLSearchParams(params as Record<string, string>)}`,
-      {
-        next: { tags: [offerTags.receivedOffers] },
-      }
-    );
-    return {
-      errorMessage: null,
-      data: { list: request.data, pagination: request.pagination },
-    };
-  } catch (error) {
-    const errorData = error as ErrorResponse;
-    return {
-      errorMessage: errorData.message,
-      data: null,
-    };
-  }
-};
-
-export const getOfferDetailAction = async (offerId: string): Promise<ResponseModel<OfferModel | null>> => {
-  try {
-    const request = await fetcher<OfferModel>(`offers/details/${offerId}`, {
-      next: { tags: [offerTags.receivedOffers] },
-    });
-    return {
-      errorMessage: null,
-      data: request,
-    };
-  } catch (error) {
-    const errorData = error as ErrorResponse;
-    return {
-      errorMessage: errorData.message,
-      data: null,
-    };
-  }
-};
-
-export const getReceivedOffersParcelNumbersAction = async (): Promise<ResponseModel<string[] | null>> => {
-  try {
-    const request = await fetcher<string[] | null>(`offers/received/parcel-numbers`, {
-      next: { tags: [offerTags.getParcelNumber] },
-    });
-
-    return {
-      data: request,
-      errorMessage: null,
-    };
-  } catch (error) {
-    const errorData = error as ErrorResponse;
-    return {
-      errorMessage: errorData.message,
-      data: null,
-    };
-  }
-};
-
-// export const revalidateReceivedOffers = async () => {
-//   const path = headers().get("referer");
-//   if (path) {
-//     revalidateTag(offerTags.receivedOffers);
-//     redirect(path);
-//   }
-// };
-
-export const deleteReceivedOffersAction = async (ids: number[]): Promise<ResponseModel<null>> => {
-  // try {
-  //   await fetcher<ResponseType<{ error: boolean }>>(`offers/received`, {
-  //     method: "DELETE",
-  //     body: JSON.stringify({ ids }),
-  //   });
-  //   // revalidateReceivedOffers();
-  //   return { data: null, errorMessage: null };
-  // } catch (error) {
-  //   const errorData = error as ErrorResponse;
-  //   return {
-  //     errorMessage: errorData.message,
-  //     data: null,
-  //   };
-  // }
-};
-
 export const createOfferAction = async (data: MakeOfferModel & { sellingPropertyId: number }): Promise<ResponseModel<null>> => {
   try {
     const req = await fetcher<ResponseType<{ error: boolean }>>(`offers`, {
@@ -131,6 +46,111 @@ export const getOfferAction = async (offerId: string): Promise<ResponseModel<Off
   }
 };
 
+/// /
+export const getReceivedOffersAction = async (params: {
+  [key: string]: string;
+}): Promise<ResponseModel<({ list: OfferModel[] } & IPagination) | null>> => {
+  try {
+    const request = await fetcher<{ data: OfferModel[] } & IPagination>(
+      `offers/received?${new URLSearchParams(params as Record<string, string>)}`,
+      {
+        next: { tags: [offerTags.receivedOffers] },
+      }
+    );
+    return {
+      errorMessage: null,
+      data: { list: request.data, pagination: request.pagination },
+    };
+  } catch (error) {
+    const errorData = error as ErrorResponse;
+    return {
+      errorMessage: errorData.message,
+      data: null,
+    };
+  }
+};
+
+export const rejectReceivedOffer = async (offerId: number): Promise<ResponseModel<null>> => {
+  try {
+    await fetcher<null>(`offers/received/reject`, {
+      body: JSON.stringify({ ids: [offerId] }),
+      method: "POST",
+    });
+    revalidateTag(offerTags.receivedOffers);
+    revalidateTag(offerTags.sentOffers);
+    revalidateTag(offerTags.getOffer);
+    return {
+      errorMessage: null,
+      data: null,
+    };
+  } catch (error) {
+    const errorData = error as ErrorResponse;
+    return {
+      errorMessage: errorData.message,
+      data: null,
+    };
+  }
+};
+
+export const acceptReceivedOffer = async (offerId: number): Promise<ResponseModel<null>> => {
+  try {
+    await fetcher<null>(`offers/received/accept`, {
+      body: JSON.stringify({ ids: [offerId] }),
+      method: "POST",
+    });
+    revalidateTag(offerTags.receivedOffers);
+    revalidateTag(offerTags.sentOffers);
+    revalidateTag(offerTags.getOffer);
+    return {
+      errorMessage: null,
+      data: null,
+    };
+  } catch (error) {
+    const errorData = error as ErrorResponse;
+    return {
+      errorMessage: errorData.message,
+      data: null,
+    };
+  }
+};
+
+export const deleteReceivedOffersAction = async (ids: number[]): Promise<ResponseModel<null>> => {
+  // try {
+  //   await fetcher<ResponseType<{ error: boolean }>>(`offers/received`, {
+  //     method: "DELETE",
+  //     body: JSON.stringify({ ids }),
+  //   });
+  //   // revalidateReceivedOffers();
+  //   return { data: null, errorMessage: null };
+  // } catch (error) {
+  //   const errorData = error as ErrorResponse;
+  //   return {
+  //     errorMessage: errorData.message,
+  //     data: null,
+  //   };
+  // }
+};
+
+export const getReceivedOffersParcelNumbersAction = async (): Promise<ResponseModel<string[] | null>> => {
+  try {
+    const request = await fetcher<string[] | null>(`offers/received/parcel-numbers`, {
+      next: { tags: [offerTags.getParcelNumber] },
+    });
+
+    return {
+      data: request,
+      errorMessage: null,
+    };
+  } catch (error) {
+    const errorData = error as ErrorResponse;
+    return {
+      errorMessage: errorData.message,
+      data: null,
+    };
+  }
+};
+
+//
 export const getSentOffersAction = async (params: {
   [key: string]: string;
 }): Promise<ResponseModel<({ list: OfferModel[] } & IPagination) | null>> => {
