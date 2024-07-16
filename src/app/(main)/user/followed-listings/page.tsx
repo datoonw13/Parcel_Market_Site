9068e014ebb7e6707afd9624845f2b94cc45685d
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-07-16 17:42:29 +0400

    fx

--- src/app/(main)/user/followed-listings/page.tsx
+++ src/app/(main)/user/followed-listings/page.tsx
@@ -4,26 +4,23 @@ import UserListing from "@/components/@new/user/listings/user-listing";
 import { Suspense } from "react";
 import UserListingLoader from "@/components/@new/user/listings/user-listing-loader";
 
-const UserFollowedListingsPage = ({ searchParams }: { searchParams: { [key: string]: string } }) => {
-
-  return (
-    <div className="w-full space-y-8">
-      <div className="flex justify-between flex-col sm:flex-row gap-6">
-        <UserProfileSectionHeader
-          title="My Saved Properties"
-          description="View and manage the properties you've bookmarked for future consideration."
-        />
-      </div>
-      <Suspense>
-        <div className="hidden sm:block">
-          <UserListingDesktopFilter />
-        </div>
-      </Suspense>
-      <Suspense fallback={<UserListingLoader />} key={JSON.stringify(searchParams)}>
-        <UserListing searchParams={searchParams} followedListings />
-      </Suspense>
+const UserFollowedListingsPage = ({ searchParams }: { searchParams: { [key: string]: string } }) => (
+  <div className="w-full space-y-8">
+    <div className="flex justify-between flex-col sm:flex-row gap-6">
+      <UserProfileSectionHeader
+        title="My Saved Properties"
+        description="View and manage the properties you've bookmarked for future consideration."
+      />
     </div>
-  );
-};
+    <Suspense>
+      <div className="hidden sm:block">
+        <UserListingDesktopFilter />
+      </div>
+    </Suspense>
+    <Suspense fallback={<UserListingLoader />} key={JSON.stringify(searchParams)}>
+      <UserListing searchParams={searchParams} followedListings />
+    </Suspense>
+  </div>
+);
 
 export default UserFollowedListingsPage;
