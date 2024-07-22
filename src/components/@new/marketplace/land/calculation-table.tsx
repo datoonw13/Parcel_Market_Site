6fdc084a39b6ce1d7b02dfdcc6c899bb6976b8c7
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-07-22 11:25:03 +0400

    feat: export data to excel

--- src/components/@new/marketplace/land/calculation-table.tsx
+++ src/components/@new/marketplace/land/calculation-table.tsx
@@ -15,6 +15,8 @@ import { saveAs } from "file-saver";
 import * as XLSX from "xlsx";
 import { numFormatter } from "@/helpers/common";
 import { ArrowIconLeft1, ArrowIconsUnion1 } from "../../icons/ArrowIcons";
+import Button from "../../shared/forms/Button";
+import { ExportIcon1 } from "../../icons/ExportIcons";
 
 const HEADER_ROWS = [
   { label: "Parcel ID", key: "parcelNumber" as const },
@@ -53,9 +55,9 @@ const LandPriceCalculationTable = ({
     setSort({ key, dir: sort.key === key && sort.dir === "desc" ? "asc" : "desc" });
   };
 
-  const exportToCSV = () => {
+  const exportToExcel = () => {
     const ws = XLSX.utils.json_to_sheet(
-      formattedData.map((el) => ({
+      sortedData.map((el) => ({
         parcelNumber: el.parcelNumber,
         county: el.county,
         arcage: el.arcage,
@@ -65,15 +67,15 @@ const LandPriceCalculationTable = ({
       }))
     );
     const maxLengthData = {
-      parcelNumber: [...formattedData].sort((a, b) => b.parcelNumber.length - a.parcelNumber.length)[0].parcelNumber.length,
-      county: [...formattedData].sort((a, b) => b.county.length - a.county.length)[0].county.length,
-      arcage: [...formattedData].sort((a, b) => b.arcage.toString().length - a.arcage.toString().length)[0].arcage.length,
-      lastSalesPrice: [...formattedData].sort((a, b) => b.lastSalesPrice!.toString().length - a.lastSalesPrice!.toString().length)[0]
+      parcelNumber: [...sortedData].sort((a, b) => b.parcelNumber.length - a.parcelNumber.length)[0].parcelNumber.length,
+      county: [...sortedData].sort((a, b) => b.county.length - a.county.length)[0].county.length,
+      arcage: [...sortedData].sort((a, b) => b.arcage.toString().length - a.arcage.toString().length)[0].arcage.length,
+      lastSalesPrice: [...sortedData].sort((a, b) => b.lastSalesPrice!.toString().length - a.lastSalesPrice!.toString().length)[0]
         .lastSalesPrice!.length,
-      pricePerAcre: [...formattedData]
+      pricePerAcre: [...sortedData]
         .sort((a, b) => b.pricePerAcre.toString().length - a.pricePerAcre.toString().length)[0]
         .pricePerAcre.toString().length,
-      lastSalesDate: [...formattedData].sort((a, b) => b.lastSalesDate!.toString().length - a.lastSalesDate!.toString().length)[0]
+      lastSalesDate: [...sortedData].sort((a, b) => b.lastSalesDate!.toString().length - a.lastSalesDate!.toString().length)[0]
         .lastSalesDate!.length,
     };
 
@@ -88,80 +90,89 @@ const LandPriceCalculationTable = ({
   };
 
   return (
-    <div className="w-full  border border-grey-100 rounded-2xl lg:min-w-96 relative">
-      <table className={clsx("w-full hidden lg:table")}>
-        <thead>
-          <tr className={clsx("bg-grey-30 rounded-2xl [&>th:first-child]:rounded-tl-2xl [&>th:last-child]:rounded-tr-2xl")}>
-            <th className="py-3 px-6 text-sm font-normal group cursor-pointer" align="left" />
-            {HEADER_ROWS.map((item) => (
-              <th className="py-3 px-6 text-sm font-normal group cursor-pointer" align="left" key={item.key}>
-                <div className="flex items-center gap-1.5" onClick={() => handleSort(item.key)}>
-                  {item.label}
-                  <ArrowIconsUnion1
-                    className={clsx(
-                      "!w-2 !h-2.5 group-hover:opacity-100 opacity-0",
-                      sort.key === item.key && sort.dir === "desc"
-                        ? "[&>path:first-child]:!fill-black [&>path:last-child]:!fill-grey-600"
-                        : "[&>path:first-child]:!fill-grey-600 [&>path:last-child]:!fill-black"
-                    )}
-                  />
-                </div>
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody
-          className="[&>tr:last-child>td:first-child]:rounded-bl-2xl [&>tr:last-child>td:last-child]:rounded-br-2xl 
-        [&>tr:not(:last-child)]:border-b [&>tr:not(:last-child)]:border-b-grey-50"
-        >
-          {sortedData.map((item, itemI) => (
-            <tr
-              key={item.latitude + item.longitude}
-              className={clsx(
-                "transition-all duration-100 cursor-pointer",
-                selectedItem === JSON.stringify([Number(item.latitude), Number(item.longitude)]) && "bg-primary-main-100",
-                hoveredItem === JSON.stringify([Number(item.latitude), Number(item.longitude)]) && "bg-primary-main-50"
-              )}
-              onClick={() => onSelect([Number(item.latitude), Number(item.longitude)])}
-              onMouseEnter={() => onMouseEnter([Number(item.latitude), Number(item.longitude)])}
-              onMouseLeave={() => onMouseLeave([Number(item.latitude), Number(item.longitude)])}
-            >
-              <td className="py-3 px-6 text-grey-800 text-xs" align="left">
-                {itemI + 1}
-              </td>
-              <td className="py-3 px-6 text-grey-800 text-xs" align="left">
-                {item.parcelNumber}
-              </td>
-              <td className="py-3 px-6 text-grey-800 text-xs" align="left">
-                {item.county ?? "--"}
-              </td>
-              <td className="py-3 px-6 text-grey-800 text-xs" align="left">
-                {item.arcage}
-              </td>
-              <td className="py-3 px-6 text-grey-800 text-xs" align="left">
-                {item.lastSalesPrice}
-              </td>
-              <td className="py-3 px-6 text-grey-800 text-xs" align="left">
-                {item.pricePerAcre}
-              </td>
-              <td className="py-3 px-6 text-grey-800 text-xs" align="left">
-                {moment(item.lastSalesDate).format("DD-MM-YYYY")}
-              </td>
+    <div>
+      <div className="w-full  border border-grey-100 rounded-2xl lg:min-w-96 relative">
+        <table className={clsx("w-full hidden lg:table")}>
+          <thead>
+            <tr className={clsx("bg-grey-30 rounded-2xl [&>th:first-child]:rounded-tl-2xl [&>th:last-child]:rounded-tr-2xl")}>
+              <th className="py-3 px-6 text-sm font-normal group cursor-pointer" align="left" />
+              {HEADER_ROWS.map((item) => (
+                <th className="py-3 px-6 text-sm font-normal group cursor-pointer" align="left" key={item.key}>
+                  <div className="flex items-center gap-1.5" onClick={() => handleSort(item.key)}>
+                    {item.label}
+                    <ArrowIconsUnion1
+                      className={clsx(
+                        "!w-2 !h-2.5 group-hover:opacity-100 opacity-0",
+                        sort.key === item.key && sort.dir === "desc"
+                          ? "[&>path:first-child]:!fill-black [&>path:last-child]:!fill-grey-600"
+                          : "[&>path:first-child]:!fill-grey-600 [&>path:last-child]:!fill-black"
+                      )}
+                    />
+                  </div>
+                </th>
+              ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <div className="w-full h-96 blur-md lg:hidden" />
-      <div className="lg:hidden absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-white border border-grey-100 rounded-2xl p-5 space-y-6">
-        <div className="relative w-16 h-14 m-auto">
-          <Image src="/no-mobile-support.png" fill alt="" className="w-16 h-14 m-auto" />
-        </div>
-        <div className="space-y-2">
-          <p className="text-sm font-medium text-center">No Mobile Support</p>
-          <p className="text-xs text-grey-600 text-center">We don’t support this information for mobile, Please open up Desktop version.</p>
+          </thead>
+          <tbody
+            className="[&>tr:last-child>td:first-child]:rounded-bl-2xl [&>tr:last-child>td:last-child]:rounded-br-2xl 
+        [&>tr:not(:last-child)]:border-b [&>tr:not(:last-child)]:border-b-grey-50"
+          >
+            {sortedData.map((item, itemI) => (
+              <tr
+                key={item.latitude + item.longitude}
+                className={clsx(
+                  "transition-all duration-100 cursor-pointer",
+                  selectedItem === JSON.stringify([Number(item.latitude), Number(item.longitude)]) && "bg-primary-main-100",
+                  hoveredItem === JSON.stringify([Number(item.latitude), Number(item.longitude)]) && "bg-primary-main-50"
+                )}
+                onClick={() => onSelect([Number(item.latitude), Number(item.longitude)])}
+                onMouseEnter={() => onMouseEnter([Number(item.latitude), Number(item.longitude)])}
+                onMouseLeave={() => onMouseLeave([Number(item.latitude), Number(item.longitude)])}
+              >
+                <td className="py-3 px-6 text-grey-800 text-xs" align="left">
+                  {itemI + 1}
+                </td>
+                <td className="py-3 px-6 text-grey-800 text-xs" align="left">
+                  {item.parcelNumber}
+                </td>
+                <td className="py-3 px-6 text-grey-800 text-xs" align="left">
+                  {item.county ?? "--"}
+                </td>
+                <td className="py-3 px-6 text-grey-800 text-xs" align="left">
+                  {item.arcage}
+                </td>
+                <td className="py-3 px-6 text-grey-800 text-xs" align="left">
+                  {item.lastSalesPrice}
+                </td>
+                <td className="py-3 px-6 text-grey-800 text-xs" align="left">
+                  {item.pricePerAcre}
+                </td>
+                <td className="py-3 px-6 text-grey-800 text-xs" align="left">
+                  {moment(item.lastSalesDate).format("DD-MM-YYYY")}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <div className="w-full h-96 blur-md lg:hidden" />
+        <div className="lg:hidden absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-white border border-grey-100 rounded-2xl p-5 space-y-6">
+          <div className="relative w-16 h-14 m-auto">
+            <Image src="/no-mobile-support.png" fill alt="" className="w-16 h-14 m-auto" />
+          </div>
+          <div className="space-y-2">
+            <p className="text-sm font-medium text-center">No Mobile Support</p>
+            <p className="text-xs text-grey-600 text-center">
+              We don’t support this information for mobile, Please open up Desktop version.
+            </p>
+          </div>
         </div>
       </div>
-      <button onClick={exportToCSV}>exportt</button>
+      <div className="hidden lg:flex gap-3 mt-4 justify-end">
+        <Button startIcon={ExportIcon1} onClick={exportToExcel}>
+          Export Data
+        </Button>
+      </div>
     </div>
   );
 };
