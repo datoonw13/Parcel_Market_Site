4fe1d9cb6e956b2dd0699f9c155278bcd576c5b6
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-07-24 18:37:05 +0400

    fix: fix subscription page styles

--- src/components/@new/subscriptions/subscriptions.tsx
+++ src/components/@new/subscriptions/subscriptions.tsx
@@ -1,5 +1,15 @@
-import { CheckIcon4 } from "../icons/CheckIcons";
+import { CheckIcon2, CheckIcon3, CheckIcon4 } from "../icons/CheckIcons";
 import Button from "../shared/forms/Button";
+
+enum SubscriptionsEnum {
+  FREE,
+  MONTHLY,
+  ANNUAL,
+}
+
+const currentPlan = SubscriptionsEnum.MONTHLY;
+
+const isSelected = (plan: SubscriptionsEnum) => currentPlan === plan;
 
 const Subscriptions = () => (
   <div className="space-y-6 sm:space-y-8 md:space-y-12 lg:space-y-16">
@@ -73,10 +83,18 @@ const Subscriptions = () => (
     </div>
     <div className="space-y-6">
       <h2 className="hidden sm:block text-center text-lg font-semibold">How Would You like to Pay?</h2>
-      <div className="grid gap-4 grid-cols-1 sm:grid-cols-3">
+      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
         <div className="space-y-8 p-4 md:p-6 lg:8 border border-grey-100 rounded-2xl">
           <div className="space-y-4 ">
-            <h2 className="text-primary-main text-lg md:text-xl lg:text-2xl font-semibold">14 Days</h2>
+            <div className="flex items-center justify-between gap-2">
+              <div className="flex items-center gap-2">
+                <h2 className="text-primary-main text-lg md:text-xl lg:text-2xl font-semibold">14 Days</h2>
+                <div className="w-6 h-6 rounded-full bg-primary-main p-1">
+                  <CheckIcon3 color="white" />
+                </div>
+              </div>
+              {isSelected(SubscriptionsEnum.FREE) && <p className="text-sm text-grey-600 font-medium">Active Until 22/09/2024</p>}
+            </div>
             <div className="space-y-1">
               <h3 className="font-semibold text-lg">Free</h3>
               <p className="font-medium text-sm">
@@ -84,21 +102,41 @@ const Subscriptions = () => (
               </p>
             </div>
           </div>
-          <Button className="w-full">Subscribe</Button>
+          <Button className="w-full" variant={isSelected(SubscriptionsEnum.FREE) ? "secondary" : "primary"}>
+            {isSelected(SubscriptionsEnum.FREE) ? "Cancel Subscription" : "Subscribe"}
+          </Button>
         </div>
         <div className="space-y-8 p-4 md:p-6 lg:8 border border-grey-100 rounded-2xl">
           <div className="space-y-4 ">
-            <h2 className="text-primary-main text-lg md:text-xl lg:text-2xl font-semibold">Monthly</h2>
+            <div className="flex items-center justify-between gap-2">
+              <div className="flex items-center gap-2">
+                <h2 className="text-primary-main text-lg md:text-xl lg:text-2xl font-semibold">Monthly</h2>
+                <div className="w-6 h-6 rounded-full bg-primary-main p-1">
+                  <CheckIcon3 color="white" />
+                </div>
+              </div>
+              {isSelected(SubscriptionsEnum.MONTHLY) && <p className="text-sm text-grey-600 font-medium">Active Until 22/09/2024</p>}
+            </div>
             <div className="space-y-1">
               <h3 className="font-semibold text-lg">$20.00</h3>
               <p className="text-primary-main text-sm font-medium">Per Month</p>
             </div>
           </div>
-          <Button className="w-full">Subscribe</Button>
+          <Button className="w-full" variant={isSelected(SubscriptionsEnum.MONTHLY) ? "secondary" : "primary"}>
+            {isSelected(SubscriptionsEnum.MONTHLY) ? "Cancel Subscription" : "Subscribe"}
+          </Button>
         </div>
-        <div className="space-y-8 p-4 md:p-6 lg:8 border border-grey-100 rounded-2xl">
+        <div className="space-y-8 p-4 md:p-6 lg:8 border border-grey-100 rounded-2xl sm:col-span-2 lg:col-auto m-auto sm:max-w-[75%] lg:max-w-full w-full">
           <div className="space-y-4 ">
-            <h2 className="text-primary-main text-lg md:text-xl lg:text-2xl font-semibold">Annual</h2>
+            <div className="flex items-center justify-between gap-2">
+              <div className="flex items-center gap-2">
+                <h2 className="text-primary-main text-lg md:text-xl lg:text-2xl font-semibold">Annual</h2>
+                <div className="w-6 h-6 rounded-full bg-primary-main p-1">
+                  <CheckIcon3 color="white" />
+                </div>
+              </div>
+              {isSelected(SubscriptionsEnum.ANNUAL) && <p className="text-sm text-grey-600 font-medium">Active Until 22/09/2024</p>}
+            </div>
             <div className="space-y-1">
               <h3 className="font-semibold text-lg">
                 $215.00<span className="text-xs font-medium text-grey-800">/17.90 per month</span>
@@ -108,7 +146,9 @@ const Subscriptions = () => (
               </p>
             </div>
           </div>
-          <Button className="w-full">Subscribe</Button>
+          <Button className="w-full" variant={isSelected(SubscriptionsEnum.ANNUAL) ? "secondary" : "primary"}>
+            {isSelected(SubscriptionsEnum.ANNUAL) ? "Cancel Subscription" : "Subscribe"}
+          </Button>
         </div>
       </div>
     </div>
