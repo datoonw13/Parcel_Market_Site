4b5fa02f4747f7250161ea4a673ad3333724f01a
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-07-25 14:19:47 +0400

    fix lint errors

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,7 +1,6 @@
 import Payment from "@/components/@new/payment/payment";
 import MiniLayout from "@/components/@new/shared/mini-layout";
 
-
 const CheckoutPage = () => (
   <MiniLayout>
     <Payment />
--- src/app/subscription/page.tsx
+++ src/app/subscription/page.tsx
@@ -1,6 +1,12 @@
 import MiniLayout from "@/components/@new/shared/mini-layout";
 import Subscription from "@/components/@new/subscription/subscription";
 
-const SignInSubscription = () => <MiniLayout><div className="rounded-2xl lg:bg-white lg:p-12 xl:p-16"><Subscription /></div></MiniLayout>;
+const SignInSubscription = () => (
+  <MiniLayout>
+    <div className="rounded-2xl lg:bg-white lg:p-12 xl:p-16">
+      <Subscription />
+    </div>
+  </MiniLayout>
+);
 
 export default SignInSubscription;
