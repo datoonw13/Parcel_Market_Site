f322ec25d3ef9cb3f4a8c5c66e09122eed106fa0
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-08-15 15:06:36 +0400

    fx

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,16 +1,20 @@
 import { AppBar } from "@/components/app-bar";
 import { Footer } from "@/components/footer";
 import SubscribeNow from "@/components/shared/SubscribeNow";
+import EventSourceListener from "@/hooks/useEventSourceListener";
 import { ReactElement } from "react";
 
 const MainLayout = ({ children }: { children: ReactElement }) => (
-  <div className="flex flex-col">
-    <AppBar />
-    {children}
-    <SubscribeNow />
-    <div className="bg-white py-20 md:py-20.5">
-      <Footer />
+  <>
+    <EventSourceListener />
+    <div className="flex flex-col">
+      <AppBar />
+      {children}
+      <SubscribeNow />
+      <div className="bg-white py-20 md:py-20.5">
+        <Footer />
+      </div>
     </div>
-  </div>
+  </>
 );
 export default MainLayout;
