a547c3485305c5191c628c58753d031b28145855
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-08-18 14:39:46 +0400

    feat: hide my followed listing page for unsubscribed user

--- src/app/(main)/user/followed-listings/page.tsx
+++ src/app/(main)/user/followed-listings/page.tsx
@@ -1,28 +1,38 @@
 import UserProfileSectionHeader from "@/components/@new/user/UserProfileSectionHeader";
 import UserListingDesktopFilter from "@/components/@new/user/listings/user-listing-desktop-filters";
-import UserListing from "@/components/@new/user/listings/user-listing";
 import { Suspense } from "react";
 import UserListingLoader from "@/components/@new/user/listings/user-listing-loader";
 import FollowedListings from "@/components/@new/user/followed-listings/followed-listings";
+import { getUserAction } from "@/server-actions/user/actions";
+import SubscribeError from "@/components/@new/shared/subscribe-error";
 
-const UserFollowedListingsPage = ({ searchParams }: { searchParams: { [key: string]: string } }) => (
-  <div className="w-full space-y-8">
-    <div className="flex justify-between flex-col sm:flex-row gap-6">
-      <UserProfileSectionHeader
-        title="My Saved Properties"
-        description="View and manage the properties you've bookmarked for future consideration."
-      />
-    </div>
-    <Suspense>
-      <div className="hidden sm:block">
-        <UserListingDesktopFilter />
+const UserFollowedListingsPage = async ({ searchParams }: { searchParams: { [key: string]: string } }) => {
+  const user = await getUserAction();
+  return (
+    <div className="w-full space-y-8">
+      <div className="flex justify-between flex-col sm:flex-row gap-6">
+        <UserProfileSectionHeader
+          title="My Saved Properties"
+          description="View and manage the properties you've bookmarked for future consideration."
+        />
       </div>
-    </Suspense>
-    <Suspense fallback={<UserListingLoader />} key={JSON.stringify(searchParams)}>
-      {/* <UserListing searchParams={searchParams} followedListings /> */}
-      <FollowedListings searchParams={searchParams} />
-    </Suspense>
-  </div>
-);
+      {user?.isSubscribed ? (
+        <>
+          <Suspense>
+            <div className="hidden sm:block">
+              <UserListingDesktopFilter />
+            </div>
+          </Suspense>
+          <Suspense fallback={<UserListingLoader />} key={JSON.stringify(searchParams)}>
+            {/* <UserListing searchParams={searchParams} followedListings /> */}
+            <FollowedListings searchParams={searchParams} />
+          </Suspense>
+        </>
+      ) : (
+        <SubscribeError />
+      )}
+    </div>
+  );
+};
 
 export default UserFollowedListingsPage;
