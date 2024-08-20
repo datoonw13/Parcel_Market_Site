fee41218243c187de2b7ced91d8442fc2a607c95
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-08-20 23:41:50 +0400

    fix ui bugs

--- src/components/@new/marketplace/list/filters/marketplace-filters-wrapper.tsx
+++ src/components/@new/marketplace/list/filters/marketplace-filters-wrapper.tsx
@@ -1,20 +1,24 @@
+"use client";
+
 import { SortEnum } from "@/types/common";
-import { getMarketplaceListAction } from "@/server-actions/marketplace/action";
-import { getUserAction } from "@/server-actions/user/actions";
 import Sort from "@/components/@new/shared/filters/Sort";
 import { IDecodedAccessToken } from "@/types/auth";
+import useMediaQuery from "@/hooks/useMediaQuery";
 import MarketplaceDesktopFilters from "./marketplace-desktop-filters";
 
-const MarketPlaceFiltersWrapper = ({ totalLends, user }: { totalLends: number; user: IDecodedAccessToken | null }) => (
-  <div className="mb-6 md:mb-8 lg:mb-10">
-    <div className="flex justify-between items-center gap-60">
-      <MarketplaceDesktopFilters disabled={!user?.isSubscribed} />
-      <div className="flex items-center gap-3 justify-end min-w-max">
-        <p className="text-grey-600 text-xs">{totalLends} Properties</p>
-        <Sort disabled={!user?.isSubscribed} options={SortEnum} />
+const MarketPlaceFiltersWrapper = ({ totalLends, user }: { totalLends: number; user: IDecodedAccessToken | null }) => {
+  const isSmallDevice = useMediaQuery(1024);
+  return (
+    <div className="mb-6 md:mb-8 lg:mb-10">
+      <div className="flex justify-between items-center gap-5 lg:gap-32 xl:gap-60">
+        {isSmallDevice ? "" : <MarketplaceDesktopFilters disabled={!user?.isSubscribed} />}
+        <div className="flex items-center gap-3 justify-end min-w-max">
+          <p className="text-grey-600 text-xs">{totalLends} Properties</p>
+          <Sort disabled={!user?.isSubscribed} options={SortEnum} />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default MarketPlaceFiltersWrapper;
