21e927df4e1c6f31a3f2c87f4fedbd298b255b3c
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-08-23 01:22:25 +0400

    fix: lint errors

--- src/components/@new/user/offers/details/offer-detail-map.tsx
+++ src/components/@new/user/offers/details/offer-detail-map.tsx
@@ -9,24 +9,21 @@ const Map = dynamic(() => import("@/components/shared/map/Map"), { ssr: false })
 interface OfferDetailMapProps {
   data: SellingPropertyDetails;
 }
-const OfferDetailMap: FC<OfferDetailMapProps> = ({ data }) => {
-  const mapOptions = {
-    data: [
-      {
-        centerCoordinate: [Number(data.lat), Number(data.lon)] as any,
-        polygon: JSON.parse(data.coordinates),
-        owner: data.owner,
-        parcelNumber: data.parcelNumber,
-        showMarker: true,
-        markerColor: "default" as const,
-      },
-    ],
-  };
-  return (
-    <div className="w-full h-44 sm:h-48 md:h-72 rounded-2xl [&>.leaflet-container]:rounded-2xl mb-3">
-      <Map data={mapOptions.data} geolibInputCoordinates={[[Number(data.lat), Number(data.lon)]]} zoom={13} />
-    </div>
-  );
-};
+const OfferDetailMap: FC<OfferDetailMapProps> = ({ data }) => (
+  <div className="w-full h-44 sm:h-48 md:h-72 rounded-2xl [&>.leaflet-container]:rounded-2xl mb-3">
+    <Map
+      properties={[
+        {
+          latitude: Number(data.lat || 1),
+          longitude: Number(data.lon || 1),
+          parcelNumber: data.parcelNumber,
+          center: true,
+          polygon: JSON.parse(data.coordinates),
+        },
+      ]}
+      zoom={13}
+    />
+  </div>
+);
 
 export default OfferDetailMap;
