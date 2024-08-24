950cb57e26ea2ac848d8ca2db73f107d983017b1
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-08-24 17:05:01 +0400

    fix(notifications): unread notifications counter

--- src/components/@new/user/notification/header/user-header-notifications-wrapper.tsx
+++ src/components/@new/user/notification/header/user-header-notifications-wrapper.tsx
@@ -3,7 +3,7 @@ import UserHeaderNotifications from "./user-header-notifications";
 
 const UserHeaderNotificationWrapper = async () => {
   const { data, errorMessage } = await getNotificationsAction({ page: "1", pageSize: "6" });
-  return <UserHeaderNotifications data={{ list: data?.list || null, unread: data?.pagination.totalCount || 0 }} />;
+  return <UserHeaderNotifications data={{ list: data?.list || null, unread: data?.pagination.unreadCount || 0 }} />;
 };
 
 export default UserHeaderNotificationWrapper;
