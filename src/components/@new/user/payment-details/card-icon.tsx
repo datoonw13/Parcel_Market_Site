9114c0ce2b28775dd5b16a11548cafd829d8f94f
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-08-25 19:39:34 +0400

    fix(payemnts): fix card logos
    :skip:

--- src/components/@new/user/payment-details/card-icon.tsx
+++ src/components/@new/user/payment-details/card-icon.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import React from "react";
 
 const CardIcon = ({ card }: { card: "visa" | "mastercard" | "amex" }) => (
-  <div className={clsx("relative", card === "visa" && " w-8 h-2.5", card === "mastercard" && "w-7 h-3", card === "amex" && "w-10 h-10")}>
+  <div className={clsx("relative", "w-14 h-9")}>
     <Image src={`/${card}.png`} fill alt={card} className="w-full h-full absolute" />
   </div>
 );
