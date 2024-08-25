7f6c77875a9a5a944e4aebf75f3af444af1707c7
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-08-25 13:10:51 +0400

    fix(responsive-header-menu): make content scrollable
    :skip:

--- src/components/app-bar/ResponsiveHeaderMenu.tsx
+++ src/components/app-bar/ResponsiveHeaderMenu.tsx
@@ -22,9 +22,9 @@ const ResponsiveHeaderMenu = ({ rootId, user }: { rootId: string; user: IDecoded
     setOpen(!open);
     const parentEl = document.getElementById(rootId);
     if (!open && rootEl.current && parentEl) {
-      rootEl.current.style.transform = `translateY(${
-        parentEl.getBoundingClientRect().top + parentEl.getBoundingClientRect().bottom - 1
-      }px)`;
+      const headerHeight = parentEl.getBoundingClientRect().top + parentEl.getBoundingClientRect().bottom - 1
+      rootEl.current.style.transform = `translateY(${headerHeight}px)`;
+      rootEl.current.style.height = `calc(100vh - ${headerHeight}px)`;
     }
   }, [open, rootId]);
 
