597b6f990474e5d999c8d5c404589ec3a1d3e893
LGabelashvili <lashagabelashvili@medicloud.ge>
2024-09-03 22:54:38 +0400

    feat(map): add layers control

--- src/components/shared/map/Map.tsx
+++ src/components/shared/map/Map.tsx
@@ -6,7 +6,7 @@ import "leaflet-defaulticon-compatibility";
 import { Button } from "@mui/material";
 import { Icon, Map as LeafletMap, Marker as LeafletMaker, LatLngExpression } from "leaflet";
 import { Fragment, ReactElement, ReactNode } from "react";
-import { FeatureGroup, MapContainer, Marker, Polygon, PolygonProps, Popup, TileLayer } from "react-leaflet";
+import { FeatureGroup, LayerGroup, LayersControl, MapContainer, Marker, Polygon, PolygonProps, Popup, TileLayer } from "react-leaflet";
 import { getCenter } from "geolib";
 
 interface IProps {
@@ -91,6 +91,41 @@ const Map = ({
       }}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {/* <LayersControl>
+        <LayersControl.BaseLayer name="Open Street Map">
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </LayersControl.BaseLayer>
+
+        <LayersControl.BaseLayer name="Mapbox Map">
+          <TileLayer
+            attribution='&copy; <a href="https://www.mapbox.com">Mapbox</a> '
+            url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}"
+            accessToken="your token here"
+          />
+        </LayersControl.BaseLayer>
+
+        <LayersControl.BaseLayer name="Mapbox Map Satellite">
+          <TileLayer
+            attribution='&copy; <a href="https://www.mapbox.com">Mapbox</a> '
+            url="https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}"
+            accessToken="your token here"
+          />
+        </LayersControl.BaseLayer>
+
+        <LayersControl.BaseLayer checked name="Google Map">
+          <TileLayer attribution="Google Maps" url="https://www.google.cn/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}" />
+        </LayersControl.BaseLayer>
+
+        <LayersControl.BaseLayer name="Google Map Satellite">
+          <LayerGroup>
+            <TileLayer attribution="Google Maps Satellite" url="https://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}" />
+            <TileLayer url="https://www.google.cn/maps/vt?lyrs=y@189&gl=cn&x={x}&y={y}&z={z}" />
+          </LayerGroup>
+        </LayersControl.BaseLayer>
+      </LayersControl> */}
       <FeatureGroup pathOptions={{ color: "#F44D61", fillColor: "#F44D61", fillOpacity: 0.2, weight: 1 }}>
         {properties.map((mapItem) => (
           <Fragment key={mapItem.parcelNumber}>
