ac7edc4ad7b7ae291b5a19964ac0f83726363746
lgabelashvili <gabelashvili1999@gmail.com>
2024-10-03 13:26:14 +0400

    refactor components

--- src/components/volt/volt-mobile/volt-mobile-header.tsx
+++ src/components/volt/volt-mobile/volt-mobile-header.tsx
@@ -1,4 +1,6 @@
 import ResponsiveHeaderMenu from "@/components/app-bar/ResponsiveHeaderMenu";
+import HeaderMenu from "@/components/landing/header/menu";
+import { Popover, PopoverAnchor } from "@/components/ui/popover";
 import Logo from "@/icons/Logo";
 import { IDecodedAccessToken } from "@/types/auth";
 import { VoltSteps } from "@/types/volt";
@@ -7,15 +9,20 @@ import React from "react";
 import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
 
 const VoltMobileHeader = ({ step, user, goBack }: { goBack: () => void; step: VoltSteps; user: IDecodedAccessToken | null }) => (
-  <div className="w-full" id="volt-navbar">
-    <div className="px-5 py-6 border-b border-b-grey-100 flex justify-between items-center">
-      {step !== VoltSteps.SEARCH && <MdOutlineKeyboardArrowLeft onClick={goBack} className="size-6" />}
-      <Link href="/">
-        <Logo className="w-[84px] h-6 sm:w-24 sm:h-8" />
-      </Link>
-      <ResponsiveHeaderMenu user={user} rootId="volt-navbar" />
-    </div>
-  </div>
+  <Popover modal>
+    <PopoverAnchor>
+      <div className="w-full" id="volt-navbar">
+        <div className="px-5 py-6 border-b border-b-grey-100 flex justify-between items-center">
+          {step !== VoltSteps.SEARCH && <MdOutlineKeyboardArrowLeft onClick={goBack} className="size-6" />}
+          <Link href="/">
+            <Logo className="w-[84px] h-6 sm:w-24 sm:h-8" />
+          </Link>
+          {/* <ResponsiveHeaderMenu user={user} rootId="volt-navbar" /> */}
+          <HeaderMenu user={user} />
+        </div>
+      </div>
+    </PopoverAnchor>
+  </Popover>
 );
 
 export default VoltMobileHeader;
