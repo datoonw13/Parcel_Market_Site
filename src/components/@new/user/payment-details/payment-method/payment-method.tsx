a21c41474151cb17a63f49f122dca995f210109d
lgabelashvili <gabelashvili1999@gmail.com>
2024-10-14 13:42:41 +0400

    fix(payment-details): default card select

--- src/components/@new/user/payment-details/payment-method/payment-method.tsx
+++ src/components/@new/user/payment-details/payment-method/payment-method.tsx
@@ -10,6 +10,8 @@ import { RemoveIcon1, RemoveIcon2 } from "@/components/@new/icons/RemoveIcons";
 import { addPaymentMethodAction, removePaymentMethodsAction } from "@/server-actions/subscription/actions";
 import ResponsiveWarningModal from "@/components/@new/shared/modals/ResponsiveWarningModal";
 import useNotification from "@/hooks/useNotification";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { TextInput } from "@/components/ui/input";
 import PaymentDetailsItem from "../payment-details-item";
 import CardIcon from "../card-icon";
 
@@ -67,60 +69,84 @@ const PaymentMethods = ({ initialData }: { initialData: (IStripePaymentMethods[0
           buttonLabel="Edit"
           onClick={() => setEdit(true)}
         />
-        <div className="grid gird-cols-1 sm:grid-cols-2 gap-3 gap-x-8">
-          {data
-            ?.filter((x) => !x.deleted)
-            .map((el) => (
-              <div key={el.id} className="flex items-center gap-3">
-                <RadioButton
-                  rootClassName={clsx("w-full", !edit && "[&>input]:hidden")}
-                  labelClassName="w-full"
-                  label={
-                    <TextField
-                      disabled
-                      variant="primary"
-                      value={`***** **** **** ${el.last4}`}
-                      label={el.name}
-                      endIcon={<CardIcon card={el.brand} />}
-                    />
-                  }
-                  checked={el.isDefault}
-                  name={el.id}
-                  onChange={() => {
-                    const newData = data.map((item) => ({ ...item, isDefault: item.id === el.id }));
-                    setData([...newData]);
-                  }}
+        {!edit && (
+          <div className="grid gird-cols-1 sm:grid-cols-2 gap-3 gap-x-8">
+            {data
+              ?.filter((x) => !x.deleted)
+              .map((el) => (
+                <TextInput
+                  key={el.id}
+                  disabled
+                  value={`***** **** **** ${el.last4}`}
+                  label={el.name}
+                  endIcon={<CardIcon card={el.brand} />}
                 />
-                {edit && (
-                  <Button
-                    onClick={() => {
-                      setRemoveItem(el);
-                    }}
-                    variant="secondary"
-                    className="!outline-none !p-0 w-8 h-8 rounded-full hover:bg-error-100 flex items-center justify-center group !bg-white"
-                  >
-                    <RemoveIcon1 className="fill-error" />
-                  </Button>
-                )}
-              </div>
-            ))}
-        </div>
-        {edit && (
-          <div className="flex items-center sm:justify-end gap-3 mt-1 border-t border-t-grey-100 pt-4 pb-1">
-            <Button
-              variant="secondary"
-              className="w-full sm:w-fit"
-              onClick={() => {
-                setEdit(false);
-                setData(initialData);
-              }}
-            >
-              Cancel
-            </Button>
-            <Button className="w-full sm:w-fit" onClick={handleSave} loading={pending}>
-              Save Changes
-            </Button>
+              ))}
           </div>
+        )}
+
+        {edit && (
+          <>
+            <RadioGroup
+              value={data.find((el) => el.isDefault)?.id || ""}
+              onValueChange={(newValueId) => {
+                const newData = data.map((el) => ({ ...el, isDefault: el.id === newValueId }));
+                setData(newData);
+              }}
+              className="grid gird-cols-1 sm:grid-cols-2 gap-3 gap-x-8"
+            >
+              {data
+                ?.filter((x) => !x.deleted)
+                .map((el) => (
+                  <div key={el.id} className="flex items-center gap-3 w-full">
+                    <RadioGroupItem
+                      label={
+                        <div className="relative">
+                          <TextInput
+                            disabled
+                            value={`***** **** **** ${el.last4}`}
+                            label={el.name}
+                            endIcon={<CardIcon card={el.brand} />}
+                          />
+                          <div className="w-full h-full rounded-2xl bg-error/0 z-10 absolute top-0" />
+                        </div>
+                      }
+                      value={el.id}
+                      onChange={() => {
+                        const newData = data.map((item) => ({ ...item, isDefault: item.id === el.id }));
+                        setData([...newData]);
+                      }}
+                    />
+                    {edit && (
+                      <Button
+                        onClick={() => {
+                          setRemoveItem(el);
+                        }}
+                        variant="secondary"
+                        className="!outline-none !p-0 w-8 h-8 rounded-full hover:bg-error-100 flex items-center justify-center group !bg-white"
+                      >
+                        <RemoveIcon1 className="fill-error" />
+                      </Button>
+                    )}
+                  </div>
+                ))}
+            </RadioGroup>
+            <div className="flex items-center sm:justify-end gap-3 mt-1 border-t border-t-grey-100 pt-4 pb-1">
+              <Button
+                variant="secondary"
+                className="w-full sm:w-fit"
+                onClick={() => {
+                  setEdit(false);
+                  setData(initialData);
+                }}
+              >
+                Cancel
+              </Button>
+              <Button className="w-full sm:w-fit" onClick={handleSave} loading={pending}>
+                Save Changes
+              </Button>
+            </div>
+          </>
         )}
       </div>
     </>
