31883d3a8a524eab0a9532ace2f964727e7c7444
lgabelashvili <gabelashvili1999@gmail.com>
2024-10-14 13:45:18 +0400

    fix(volt): footer links

--- src/components/volt/volt-footer.tsx
+++ src/components/volt/volt-footer.tsx
@@ -1,14 +1,15 @@
+import routes from "@/helpers/routes";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 
 const VoltFooter = ({ className }: { className?: string }) => (
   <div className={cn("flex items-center justify-between", className)}>
     <div className="flex gap-3 items-center justify-center lg:justify-start">
-      <Link href="/">
+      <Link href={routes.privacyPolicy.fullUrl}>
         <p className="text-sm text-gray-800">Privacy Policy</p>
       </Link>
       <div className="w-[1px] h-4 bg-gray-200" />
-      <Link href="/">
+      <Link href={routes.termsConditions.fullUrl}>
         <p className="text-sm text-gray-800">Terms of use</p>
       </Link>
     </div>
