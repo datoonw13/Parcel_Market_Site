fdd0edcf69f321a5dca7e99e2bc065be2585ffc4
lgabelashvili <gabelashvili1999@gmail.com>
2024-10-17 13:28:25 +0400

    fix ui bugs

--- src/components/landing/questions.tsx
+++ src/components/landing/questions.tsx
@@ -5,7 +5,7 @@ import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "..
 import { Button } from "../ui/button";
 
 const LandingQuestions = () => (
-  <div className="space-y-6 md:space-y-8 lg:space-y-11 px-5 sm:px-8 md:px-11 lg:px-16 xl:px-[11vw] ">
+  <div className=" flex flex-col gap-6 md:gap-8 lg:gap-11 px-5 sm:px-8 md:px-11 lg:px-16 xl:px-[11vw] ">
     <div className="space-y-2 md:space-y-3 max-w-4xl mx-auto">
       <h1 className="text-center font-extrabold text-2xl md:text-3xl lg:text-4xl xl:text-5xl">Frequently asked questions</h1>
       <h2 className="text-center font-light text-sm md:text-base">
