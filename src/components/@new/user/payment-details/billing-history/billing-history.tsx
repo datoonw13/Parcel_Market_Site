7a4af6c0f0cd17329d88c3d3fd4e9240ab5ad710
lgabelashvili <gabelashvili1999@gmail.com>
2024-10-17 12:05:37 +0400

    feat(billing): show error when there is no payment

--- src/components/@new/user/payment-details/billing-history/billing-history.tsx
+++ src/components/@new/user/payment-details/billing-history/billing-history.tsx
@@ -3,10 +3,12 @@
 import { useState } from "react";
 import { getUserBillingHistoryAction } from "@/server-actions/subscription/actions";
 import { IStripeCharge } from "@/types/subscriptions";
+import useNotification from "@/hooks/useNotification";
 import PaymentDetailsItem from "../payment-details-item";
 import PaymentDetailsModal from "./payment-details-modal";
 
 const BillingHistory = () => {
+  const { notify } = useNotification();
   const [pending, setPending] = useState(false);
   const [data, setData] = useState<IStripeCharge[] | null>(null);
 
@@ -16,6 +18,8 @@ const BillingHistory = () => {
     setPending(false);
     if (data) {
       setData(data.filter((el) => el.paid));
+    } else {
+      notify({ title: "Billing history error", description: "You do not have a billing history yet" }, { variant: "error" });
     }
   };
 
