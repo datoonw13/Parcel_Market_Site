cc3d4ba104f622eecd86f13c404a61ce0e3401a4
lgabelashvili <gabelashvili1999@gmail.com>
2024-10-23 15:50:45 +0400

    fix ui bug

--- src/components/@new/shared/mini-layout.tsx
+++ src/components/@new/shared/mini-layout.tsx
@@ -2,17 +2,18 @@
 import { MiniFooter } from "@/components/footer";
 import clsx from "clsx";
 import { ReactNode } from "react";
+import { cn } from "@/lib/utils";
 import Container from "./Container";
 import HeaderMini from "./header-mini";
 
 const MiniLayout = ({ children, rootClasses, contentClasses }: { children: ReactNode; rootClasses?: string; contentClasses?: string }) => (
   <div className={clsx("flex flex-col justify-between bg-white sm:bg-grey-30", rootClasses, "space-y-8")}>
     {/* <AppBarMini /> */}
-    <Container>
+    <Container className="max-w-[1400px]">
       <HeaderMini />
     </Container>
-    <Container className={contentClasses}>{children}</Container>
-    <Container maxWidth="max-w-full" className="pb-4 sm:pb-5 md:pb-6 lg:pb-[30px]">
+    <Container className={cn(contentClasses, "max-w-[1400px]")}>{children}</Container>
+    <Container maxWidth="max-w-full" className="pb-4 sm:pb-5 md:pb-6 lg:pb-[30px] max-w-[1400px]">
       <MiniFooter />
     </Container>
   </div>
