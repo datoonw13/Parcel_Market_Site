b2534ef48c6b389633047a4bead7cd2f3291e566
lgabelashvili <gabelashvili1999@gmail.com>
2024-10-31 14:52:04 +0400

    feat(landing): add image

--- src/components/landing/about-volt.tsx
+++ src/components/landing/about-volt.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 import Link from "next/link";
 import React from "react";
 import routes from "@/helpers/routes";
@@ -19,13 +20,15 @@ const AboutVolt = () => (
         data, VOLT is able to estimate an average value for similar sized properties in a market area. You can also use VOLT to search,
         save, and export county sales data.
       </h2>
-      <Link className="w-full flex justify-center" href={routes.volt.fullUrl}>
-        <Button className="w-full md:w-fit">Try for free</Button>
-      </Link>
     </div>
 
-    {/* <div className="grid grid-cols-1 md:grid-cols-[1.2fr_1fr] items-center gap-6 md:gap-10 lg:gap-16 xl:gap-24">
-      <div className="bg-error-100 h-56 sm:h-60 md:h-72 lg:h-80 xl:h-[400px] rounded-2xl">Video</div>
+    <div className="grid grid-cols-1 md:grid-cols-[1.2fr_1fr] items-center gap-6 md:gap-10 lg:gap-16 xl:gap-24">
+      <div className="h-56 sm:h-60 md:h-72 lg:h-80 xl:h-[400px] rounded-2xl">
+        <video width="100%" height="100%" className="w-full h-full" poster="/subnail.png" controls preload="metadata">
+          <source src="/video.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
       <div className="h-fit space-y-2 md:space-y-3">
         <h1 className="font-bold text-xl sm:text-2xl md:text-3xl lg:font-extrabold lg:text-4xl xl:text-5xl">How Does Volt work?</h1>
         <h1 className="font-extralight text-sm md:text-base">
@@ -36,7 +39,7 @@ const AboutVolt = () => (
           <Button className="mt-6 md:mt-8 w-full md:w-fit">Try for free</Button>
         </Link>
       </div>
-    </div> */}
+    </div>
   </div>
 );
 
