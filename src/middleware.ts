7b87f58826005a11fde2e94dc915888d7b03a2d1
lgabelashvili <gabelashvili1999@gmail.com>
2024-11-16 22:15:22 +0400

    fx

--- src/middleware.ts
+++ src/middleware.ts
@@ -110,7 +110,7 @@ export async function middleware(request: NextRequest) {
   }
 
   try {
-    const x = await fetch(new URL("/user/token/refresh", `https://apitest.parcelmarket.com/api`), {
+    const x = await fetch(new URL("api/user/token/refresh", `https://apitest.parcelmarket.com`), {
       method: "POST",
       headers: {
         Accept: "application/json",
