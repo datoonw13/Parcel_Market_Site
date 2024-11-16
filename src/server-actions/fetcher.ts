8275c4cd7299a378eb591b8b985fb3cf90075b33
lgabelashvili <gabelashvili1999@gmail.com>
2024-11-16 22:58:34 +0400

    update api url

--- src/server-actions/fetcher.ts
+++ src/server-actions/fetcher.ts
@@ -7,7 +7,7 @@ import { revalidatePath } from "next/cache";
 // https://api.parcelmarket.com
 // eslint-disable-next-line no-undef
 export const fetcher = async <T>(url: string, options?: RequestInit): Promise<T> => {
-  const request = await fetch(`${process.env.API_URL}${url}`, {
+  const request = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${url}`, {
     ...options,
     headers: {
       "Content-Type": "application/json",
