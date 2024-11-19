1761ebdff0c4f980a573bf651b8c483ee50e1dcb
lgabelashvili <gabelashvili1999@gmail.com>
2024-11-19 17:04:09 +0400

    fx

--- src/server-actions/fetcher.ts
+++ src/server-actions/fetcher.ts
@@ -22,6 +22,8 @@ export const fetcher = async <T>(url: string, options?: RequestInit): Promise<T>
     statusCode: number;
   };
 
+  console.log(response, 11);
+
   if (!response?.statusCode?.toString()?.startsWith("2")) {
     let errorMessage = "Something went wrong";
     if (response.message) {
