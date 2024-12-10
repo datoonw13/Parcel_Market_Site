595e12375cd814b1d5222f40d83081c2ffaf22be
lgabelashvili <gabelashvili1999@gmail.com>
2024-12-10 15:54:47 +0400

    fx

--- src/app/volt/page.tsx
+++ src/app/volt/page.tsx
@@ -1,6 +1,8 @@
 import VoltWrapper from "@/components/volt/volt-wrapper";
 import { getUserAction } from "@/server-actions/user/actions";
 
+export const maxDuration = 30;
+
 const VoltPage = async () => {
   const user = await getUserAction();
   return <VoltWrapper user={user} />;
