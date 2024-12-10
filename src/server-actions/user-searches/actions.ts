243f27d73bc5327af2cc9f214d1708b38b2c4639
lgabelashvili <gabelashvili1999@gmail.com>
2024-12-10 15:53:10 +0400

    revert commit

--- src/server-actions/user-searches/actions.ts
+++ src/server-actions/user-searches/actions.ts
@@ -17,8 +17,6 @@ import { decode } from "jsonwebtoken";
 import { userSearchesTag } from "./tags";
 import { fetcher } from "../fetcher";
 
-export const maxDuration = 40;
-
 export const getUserSearches = async (
   filters: Partial<z.infer<typeof userSearchesValidations>> & { page: number; pageSize: number }
 ): Promise<ResponseModel<({ list: { title: string; id: number }[] } & IPagination) | null>> => {
