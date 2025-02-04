15ab05d5790a18a153adb93de7adb9802f020170
lgabelashvili <gabelashvili1999@gmail.com>
2025-02-04 15:22:06 +0400

    fx

--- src/components/volt-details/progress-line.tsx
+++ src/components/volt-details/progress-line.tsx
@@ -78,12 +78,7 @@ const VoltDetailsProgressLine: FC<VoltDetailsProgressLineProps> = ({
 
   return (
     // rounded-b-2xl
-    <div
-      className="border border-primary-main-400 bg-[#FAFFFB] p-5  border-t-0 space-y-12 relative z-10"
-      style={{
-        boxShadow: "rgba(0, 0, 0, 0.2) 0px -18px 48px 0px",
-      }}
-    >
+    <div className="border border-primary-main-400 bg-[#FAFFFB] py-3 px-4  border-t-0 space-y-8 relative z-10 shadow-3">
       <div className="flex items-center justify-between gap-4 border-b border-b-[#C3EBD3] pb-3">
         <div className="flex gap-4 items-center">
           <div className="flex items-center gap-2">
