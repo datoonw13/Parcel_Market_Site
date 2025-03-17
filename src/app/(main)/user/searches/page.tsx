5a9b7a635177b85a182beb96fafffe8c16eaa530
lgabelashvili <gabelashvili1999@gmail.com>
2025-03-17 22:15:04 +0400

    fx

--- src/app/(main)/user/searches/page.tsx
+++ src/app/(main)/user/searches/page.tsx
@@ -4,9 +4,10 @@ import { getUserSearches } from "@/server-actions/user-searches/actions";
 import { userSearchesValidations } from "@/zod-validations/filters-validations";
 
 const UserSearches = async ({ searchParams }: { searchParams: Record<string, string | null> }) => {
-  const data = await getUserSearches({ page: 1, pageSize: 1 });
   const filters = userSearchesValidations.safeParse(searchParams);
+  const data = await getUserSearches({ ...filters, page: 1, pageSize: 1 });
   const hasEntries = !!(data.data?.list && data.data.list.length > 0);
+
   return (
     filters.success && (
       <Suspense>
