93bfc2452c16356c780bfd828d3135ef79237d51
lgabelashvili <gabelashvili1999@gmail.com>
2025-04-07 00:39:52 +0400

    refactor property type filter

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -5,6 +5,8 @@ import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 import { FaCheck } from "react-icons/fa";
 
 import { cn } from "@/lib/utils";
+import { RiCheckboxIndeterminateFill } from "react-icons/ri";
+import { MdOutlineHorizontalRule } from "react-icons/md";
 
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
@@ -14,13 +16,14 @@ const Checkbox = React.forwardRef<
     <CheckboxPrimitive.Root
       ref={ref}
       className={cn(
-        "peer h-4 w-4 shrink-0 rounded-sm border border-grey-200 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary-main data-[state=checked]:text-white"
+        "group peer h-4 w-4 shrink-0 rounded-sm border border-grey-200 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary-main data-[state=checked]:text-white data-[state=indeterminate]:bg-primary-main data-[state=indeterminate]:text-white"
       )}
       {...props}
       id={id}
     >
       <CheckboxPrimitive.Indicator className={cn("flex items-center justify-center text-current")}>
-        <FaCheck className="h-2 w-2" />
+        <FaCheck className="h-2 w-2 hidden group-data-[state=checked]:block" />
+        <MdOutlineHorizontalRule className="h-3 w-3 hidden group-data-[state=indeterminate]:block" />
       </CheckboxPrimitive.Indicator>
     </CheckboxPrimitive.Root>
     {label && (
--- src/components/volt-details/filters/property-type.tsx
+++ src/components/volt-details/filters/property-type.tsx
@@ -4,8 +4,8 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { cn } from "@/lib/utils";
 import { voltDetailsFiltersValidations } from "@/zod-validations/filters-validations";
-import { FC } from "react";
-import { z } from "zod";
+import { FC, useEffect } from "react";
+import { boolean, z } from "zod";
 
 interface VoltDetailsPropertyTypeFiltersProps {
   selected: z.infer<typeof voltDetailsFiltersValidations>["propertyTypes"];
@@ -14,27 +14,62 @@ interface VoltDetailsPropertyTypeFiltersProps {
   className?: string;
 }
 
-const VoltDetailsPropertyTypeFilters: FC<VoltDetailsPropertyTypeFiltersProps> = ({ onChange, selected, propertyTypes, className }) => (
-  <div className={cn("flex gap-16 h-96 overflow-hidden", className)}>
-    <ScrollArea className="h-full [&>div>div:first-child]:h-full" id="volt-scroll">
-      <div className="flex flex-col max-w-72">
-        <p className="bg-white sticky top-0 z-10 mr-2 text-sm font-medium pb-2">Vacant Land</p>
-        <div className="pr-1 flex flex-col gap-1">
-          {propertyTypes
-            .filter((el) => el.group === "vacant-land")
-            .map((propertyType) => (
+const VoltDetailsPropertyTypeFilters: FC<VoltDetailsPropertyTypeFiltersProps> = ({ onChange, selected, propertyTypes, className }) => {
+  const filteredPropertyTypes = propertyTypes.filter((el) => el.group === "vacant-land");
+  const filteredSelected = selected?.map((el) => filteredPropertyTypes.find((x) => x.id === el)?.id).filter(Boolean) || [];
+
+  const selectAllState = () => {
+    if (filteredSelected?.length === 0) {
+      return false;
+    }
+
+    if (filteredPropertyTypes?.length === filteredSelected.length) {
+      return true;
+    }
+    return "indeterminate";
+  };
+
+  const handleSelectAllChange = () => {
+    if (selectAllState() === "indeterminate" || filteredSelected?.length === 0) {
+      onChange(filteredPropertyTypes.map((el) => el.id));
+    } else {
+      onChange(null);
+    }
+  };
+
+  useEffect(() => {
+    if (filteredSelected?.length === 0) {
+      onChange(filteredPropertyTypes.map((el) => el.id));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className={cn("flex gap-16 h-96 overflow-hidden", className)}>
+      <ScrollArea className="h-full [&>div>div:first-child]:h-full" id="volt-scroll">
+        <div className="flex flex-col max-w-72">
+          <p className="bg-white sticky top-0 z-10 mr-2 text-sm font-medium pb-2">Vacant Land</p>
+          <div className="pr-1 flex flex-col gap-1">
+            <Checkbox
+              className="items-start [&>button]:mt-1"
+              onCheckedChange={handleSelectAllChange}
+              checked={selectAllState()}
+              label={<span className="font-medium text-xs">Select All</span>}
+              id="selectAll"
+            />
+            {filteredPropertyTypes.map((propertyType) => (
               <Checkbox
                 className="items-start [&>button]:mt-1"
                 onCheckedChange={() => {
-                  const isChecked = selected?.includes(propertyType.id);
-                  let newSelected: any = [...(selected || [])].map((el) => Number(el));
+                  const isChecked = filteredSelected?.includes(propertyType.id);
+                  let newSelected: any = [...(filteredSelected || [])].map((el) => Number(el));
                   if (isChecked) {
                     newSelected = newSelected.filter((el: any) => el !== Number(propertyType.id));
                   } else {
                     newSelected = [...newSelected, propertyType.id];
                   }
 
-                  if (!selected) {
+                  if (!filteredSelected) {
                     newSelected = propertyTypes
                       .filter((el) => el.group === "vacant-land")
                       .filter((el) => el.id !== propertyType.id)
@@ -42,52 +77,53 @@ const VoltDetailsPropertyTypeFilters: FC<VoltDetailsPropertyTypeFiltersProps> = 
                   }
                   onChange(newSelected.length ? newSelected : null);
                 }}
-                checked={!!selected?.includes(propertyType.id) || selected?.length === 0 || !selected}
+                checked={!!filteredSelected?.includes(propertyType.id)}
                 label={<span className="font-medium text-xs">{propertyType.value}</span>}
                 key={propertyType.id}
                 id={propertyType.id.toString()}
               />
             ))}
+          </div>
         </div>
-      </div>
-    </ScrollArea>
-    {/* <ScrollArea className="h-full [&>div>div:first-child]:h-full" id="volt-scroll">
-      <div className="flex flex-col max-w-72">
-        <p className="bg-white sticky top-0 z-10 mr-2 text-sm font-medium pb-2">Others</p>
-        <div className="pr-1 flex flex-col gap-1">
-          {propertyTypes
-            .filter((el) => el.group === "other")
-            .map((propertyType) => (
-              <Checkbox
-                className="items-start [&>button]:mt-1"
-                onCheckedChange={() => {
-                  const isChecked = selected?.includes(propertyType.id);
-                  let newSelected: any = [...(selected || [])].map((el) => Number(el));
-
-                  if (!selected) {
-                    newSelected = propertyTypes
-                      .filter((el) => el.group === "vacant-land")
-                      .filter((el) => el.id !== propertyType.id)
-                      .map((el) => el.id);
-                  }
-
-                  if (isChecked) {
-                    newSelected = newSelected.filter((el: any) => el !== Number(propertyType.id));
-                  } else {
-                    newSelected = [...newSelected, propertyType.id];
-                  }
-                  onChange(newSelected.length ? newSelected : null);
-                }}
-                checked={!!selected?.includes(propertyType.id)}
-                label={<span className="font-medium text-xs">{propertyType.value}</span>}
-                key={propertyType.id}
-                id={propertyType.id.toString()}
-              />
-            ))}
+      </ScrollArea>
+      {/* <ScrollArea className="h-full [&>div>div:first-child]:h-full" id="volt-scroll">
+        <div className="flex flex-col max-w-72">
+          <p className="bg-white sticky top-0 z-10 mr-2 text-sm font-medium pb-2">Others</p>
+          <div className="pr-1 flex flex-col gap-1">
+            {propertyTypes
+              .filter((el) => el.group === "other")
+              .map((propertyType) => (
+                <Checkbox
+                  className="items-start [&>button]:mt-1"
+                  onCheckedChange={() => {
+                    const isChecked = selected?.includes(propertyType.id);
+                    let newSelected: any = [...(selected || [])].map((el) => Number(el));
+  
+                    if (!selected) {
+                      newSelected = propertyTypes
+                        .filter((el) => el.group === "vacant-land")
+                        .filter((el) => el.id !== propertyType.id)
+                        .map((el) => el.id);
+                    }
+  
+                    if (isChecked) {
+                      newSelected = newSelected.filter((el: any) => el !== Number(propertyType.id));
+                    } else {
+                      newSelected = [...newSelected, propertyType.id];
+                    }
+                    onChange(newSelected.length ? newSelected : null);
+                  }}
+                  checked={!!selected?.includes(propertyType.id)}
+                  label={<span className="font-medium text-xs">{propertyType.value}</span>}
+                  key={propertyType.id}
+                  id={propertyType.id.toString()}
+                />
+              ))}
+          </div>
         </div>
-      </div>
-    </ScrollArea> */}
-  </div>
-);
+      </ScrollArea> */}
+    </div>
+  );
+};
 
 export default VoltDetailsPropertyTypeFilters;
