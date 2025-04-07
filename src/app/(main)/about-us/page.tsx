a22665e4d25fae2761bf8e58357f685c1c9611b4
lgabelashvili <gabelashvili1999@gmail.com>
2025-04-07 09:58:57 +0400

    update about us

--- src/app/(main)/about-us/page.tsx
+++ src/app/(main)/about-us/page.tsx
@@ -14,72 +14,6 @@ const AboutUs = () => {
     <>
       {/* <Header /> */}
       <Container sx={{ py: { xs: 6, md: 8, lg: 10 }, display: "flex", flexDirection: "column", gap: 4.5 }}>
-        <Box sx={{ display: "flex", flexDirection: "column" }}>
-          {/* <Typography sx={{ fontSize: { xs: 24, sm: 28, md: 32, lg: 36 }, textAlign: "center", fontWeight: 600 }}>About us</Typography> */}
-          <Typography sx={{ fontSize: 16, textAlign: "center", mt: { xs: 1.5, sm: 2, md: 3 }, fontWeight: { md: 500 } }}>
-            Welcome to Parcel Market and thank You for being here. At Parcel Market, our goal is simple, to provide a FREE and convenient
-            place to start when making decisions regarding vacant land.{" "}
-          </Typography>
-        </Box>
-        <Box sx={{ position: "relative" }}>
-          <Box
-            sx={{
-              height: {
-                xs: 240,
-                sm: 340,
-                md: 440,
-                lg: 530,
-                position: "relative",
-                "& > img": {
-                  borderTopLeftRadius: 16,
-                  borderTopRightRadius: 16,
-                  borderBottomLeftRadius: upMd ? 16 : 0,
-                  borderBottomRightRadius: upMd ? 16 : 0,
-                },
-              },
-            }}
-          >
-            <Image alt="" src="/people.png" fill loading="eager" style={{ transform: "scaleX(-1)" }} />
-          </Box>
-          <Box
-            sx={{
-              px: 2.5,
-              py: 2,
-              p: { md: 4.5 },
-              bgcolor: "primary.200",
-              borderBottomLeftRadius: { xs: 16 },
-              borderBottomRightRadius: { xs: 16, md: 0 },
-              borderTopRightRadius: { md: 16 },
-              fontWeight: 500,
-              fontSize: { xs: 14, md: 16 },
-              position: { md: "absolute" },
-              bottom: 0,
-              width: { md: "55%" },
-            }}
-          >
-            “We strive to work closely together with our customers in solving their problems. We value honesty, integrity, and efficiency”
-          </Box>
-        </Box>
-        <Typography sx={{ fontSize: 14, color: "grey.800" }}>
-          While the information provided by Parcel Market’s Value Of land Tool (VOLT) is the main driver behind our purpose, we also wanted
-          to help our users utilize that information. We understand that the land business can be intimidating and finding qualified buyers,
-          sellers, or industry professionals can be quite the endeavor! We decided that providing options for taking the next steps after
-          determining land value is also very important. We offer three unique options for our users, and which one is right for you will
-          depend on your situation. They are The Parcel Market Purchase Option, The Parcel Market Marketplace, and Our Preferred Land
-          Specialists Network. Let’s explain these a bit further so you are well informed on how Parcel Market can help your specific needs.
-        </Typography>
-        <Box>
-          <Typography sx={{ fontSize: 18, fontWeight: 600, mb: 1.5 }}>The Parcel Market Value of Land Tool</Typography>
-          <Typography sx={{ fontSize: 14, color: "grey.800" }}>
-            While the information provided by Parcel Market’s Value Of land Tool (VOLT) is the main driver behind our purpose, we also
-            wanted to help our users utilize that information. We understand that the land business can be intimidating and finding
-            qualified buyers, sellers, or industry professionals can be quite the endeavor! We decided that providing options for taking the
-            next steps after determining land value is also very important. We offer three unique options for our users, and which one is
-            right for you will depend on your situation. They are The Parcel Market Purchase Option, The Parcel Market Marketplace, and Our
-            Preferred Land Specialists Network. Let’s explain these a bit further so you are well informed on how Parcel Market can help
-            your specific needs.
-          </Typography>
-        </Box>
         <Box
           sx={{
             display: "flex",
@@ -100,7 +34,7 @@ const AboutUs = () => {
               },
             }}
           >
-            Our 3 unique options for users
+            About Us
           </Typography>
           <Box
             sx={{
@@ -120,21 +54,13 @@ const AboutUs = () => {
                 gap: 1,
                 color: "primary.main",
               }}
-            >
-              <Typography sx={{ fontSize: 18, fontWeight: 500 }}>01.</Typography>
-              <Typography sx={{ fontSize: 18, fontWeight: 600, width: { md: "max-content" } }}>
-                The Parcel Market Purchase Option
-              </Typography>
-            </Box>
+            />
             <Typography sx={{ fontSize: 14, color: "grey.800" }}>
-              For sellers who are looking to liquidate quickly and with no closing expenses, our Parcel Market Purchase Option may be the
-              right fit. Under this option, Parcel Market will make an offer to buy your land based on the VOLT value. If accepted, we will
-              handle everything from contracts to closing! You will also be able to close remotely, at your convenience. As an added benefit
-              and peace of mind, Parcel Market will pay a third-party attorney of your choice to review the purchase agreement on your
-              behalf. While our Parcel Market Purchase Option offers the fastest way to sell your land with no closing expenses and via a
-              remote closing, it&apos;s important to note that the offer will be below the VOLT value and therefore likely below the market
-              value of your property. This option is ideal for sellers looking for a quick and hassle-free sale, prioritizing speed and
-              convenience over maximizing sale price.
+              Parcel Market was created by a team of seasoned real estate professionals and skilled software developers who share a deep
+              understanding of the land industry and a passion for building intuitive technology. Frustrated by the inefficiencies and lack
+              of reliable land sales data from county sources, our founders set out to solve a major challenge faced by real estate
+              professionals and investors: getting accurate, affordable, and accessible data on vacant land sales—beyond what’s available
+              through traditional MLS systems.
             </Typography>
           </Box>
           <Box
@@ -155,21 +81,11 @@ const AboutUs = () => {
                 gap: 1,
                 color: "primary.main",
               }}
-            >
-              <Typography sx={{ fontSize: 18, fontWeight: 500 }}>01.</Typography>
-              <Typography sx={{ fontSize: 18, fontWeight: 600, width: { md: "max-content" } }}>
-                The Parcel Market Purchase Option
-              </Typography>
-            </Box>
+            />
             <Typography sx={{ fontSize: 14, color: "grey.800" }}>
-              For sellers who are looking to liquidate quickly and with no closing expenses, our Parcel Market Purchase Option may be the
-              right fit. Under this option, Parcel Market will make an offer to buy your land based on the VOLT value. If accepted, we will
-              handle everything from contracts to closing! You will also be able to close remotely, at your convenience. As an added benefit
-              and peace of mind, Parcel Market will pay a third-party attorney of your choice to review the purchase agreement on your
-              behalf. While our Parcel Market Purchase Option offers the fastest way to sell your land with no closing expenses and via a
-              remote closing, it&apos;s important to note that the offer will be below the VOLT value and therefore likely below the market
-              value of your property. This option is ideal for sellers looking for a quick and hassle-free sale, prioritizing speed and
-              convenience over maximizing sale price.
+              We believe land data should be straightforward, easy to access, and focused on what truly drives land value. Instead of
+              overwhelming users with unnecessary features and inflated pricing, we’ve built a platform that delivers only what
+              matters—reliable data, user-friendly tools, and insights that help professionals make informed decisions without the noice.
             </Typography>
           </Box>
           <Box
@@ -190,29 +106,39 @@ const AboutUs = () => {
                 gap: 1,
                 color: "primary.main",
               }}
-            >
-              <Typography sx={{ fontSize: 18, fontWeight: 500 }}>01.</Typography>
-              <Typography sx={{ fontSize: 18, fontWeight: 600, width: { md: "max-content" } }}>
-                The Parcel Market Purchase Option
-              </Typography>
-            </Box>
+            />
             <Typography sx={{ fontSize: 14, color: "grey.800" }}>
-              For sellers who are looking to liquidate quickly and with no closing expenses, our Parcel Market Purchase Option may be the
-              right fit. Under this option, Parcel Market will make an offer to buy your land based on the VOLT value. If accepted, we will
-              handle everything from contracts to closing! You will also be able to close remotely, at your convenience. As an added benefit
-              and peace of mind, Parcel Market will pay a third-party attorney of your choice to review the purchase agreement on your
-              behalf. While our Parcel Market Purchase Option offers the fastest way to sell your land with no closing expenses and via a
-              remote closing, it&apos;s important to note that the offer will be below the VOLT value and therefore likely below the market
-              value of your property. This option is ideal for sellers looking for a quick and hassle-free sale, prioritizing speed and
-              convenience over maximizing sale price.
+              At Parcel Market, we stand by our core values: integrity, transparency, and community. As a dedicated team, we’re committed to
+              building a trusted, easy-to-use platform that empowers land professionals and investors nationwide. We continuously refine our
+              tools to enhance accuracy, usability, and overall experience—while keeping our subscription pricing accessible.
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              gap: { xs: 1.5, md: 12 },
+              justifyContent: { md: "space-between" },
+              flexDirection: { xs: "column", md: "row" },
+              alignItems: { md: "start" },
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: { xs: "column", md: "row" },
+                justifyContent: "center",
+                alignItems: "center",
+                gap: 1,
+                color: "primary.main",
+              }}
+            />
+            <Typography sx={{ fontSize: 14, color: "grey.800" }}>
+              We’re excited about the road ahead as we keep growing and evolving Parcel Market into the essential resource for anyone
+              looking to understand and invest in vacant land.
             </Typography>
           </Box>
         </Box>
       </Container>
-      {/* <SubscribeNow />
-      <Box sx={{ bgcolor: "white", py: { xs: 10, md: 11, lg: 12.5 } }}>
-        <Footer />
-      </Box> */}
     </>
   );
 };
