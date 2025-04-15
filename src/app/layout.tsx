831979baa676bac72acc919dc66f3fc38b8298b7
lgabelashvili <gabelashvili1999@gmail.com>
2025-04-15 14:37:31 +0400

    test commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,7 +60,7 @@ export default async function RootLayout({
 
   return (
     <>
-      <html lang="en">
+      <html lang="en" className="h-full">
         {process.env.NODE_ENV === "production" && (
           <>
             <Script id="fb-pixel" strategy="afterInteractive">
@@ -91,7 +91,7 @@ export default async function RootLayout({
         )}
         <GoogleTagManager gtmId="GTM-P59N8LFM" />
         <GoogleAnalytics gaId="G-SBBPRZKYR6" />
-        <body className={clsx(inter.className, inter.variable, bricolage.variable, "h-dvh")}>
+        <body className={clsx(inter.className, inter.variable, bricolage.variable, "min-h-full flex flex-col")}>
           {process.env.NODE_ENV === "production" && (
             <noscript>
               <Image
